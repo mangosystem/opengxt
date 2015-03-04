eed7a74d27b14fc3d3a838f1d2b7a8685fb1007d
Minpa Lee <mapplus@gmail.com>
2015-03-04 13:05:27 +0900

    added kernel density process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/KernelDensityProcess.java
@@ -0,0 +1,166 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.media.jai.JAI;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterKernelDensityOperation;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterKernelDensityOperation.KernelType;
+import org.geotools.text.Text;
+import org.geotools.util.NullProgressListener;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Calculates a magnitude per unit area from point features using a kernel function to fit a smoothly tapered surface to each point.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class KernelDensityProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(KernelDensityProcess.class);
+
+    private boolean started = false;
+
+    public KernelDensityProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static GridCoverage2D process(SimpleFeatureCollection inputFeatures,
+            KernelType kernelType, String populationField, Double searchRadius, Double cellSize,
+            ReferencedEnvelope extent, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(KernelDensityProcessFactory.inputFeatures.key, inputFeatures);
+        map.put(KernelDensityProcessFactory.kernelType.key, kernelType);
+        map.put(KernelDensityProcessFactory.populationField.key, populationField);
+        map.put(KernelDensityProcessFactory.searchRadius.key, searchRadius);
+        map.put(KernelDensityProcessFactory.cellSize.key, cellSize);
+        map.put(KernelDensityProcessFactory.extent.key, extent);
+
+        Process process = new KernelDensityProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (GridCoverage2D) resultMap.get(KernelDensityProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        if (started)
+            throw new IllegalStateException("Process can only be run once");
+        started = true;
+
+        if (monitor == null)
+            monitor = new NullProgressListener();
+        try {
+            monitor.started();
+            monitor.setTask(Text.text("Grabbing arguments"));
+            monitor.progress(10.0f);
+
+            SimpleFeatureCollection inputFeatures = (SimpleFeatureCollection) Params.getValue(
+                    input, KernelDensityProcessFactory.inputFeatures, null);
+            if (inputFeatures == null) {
+                throw new NullPointerException("inputFeatures parameters required");
+            }
+
+            KernelType kernelType = (KernelType) Params.getValue(input,
+                    KernelDensityProcessFactory.kernelType, KernelType.QUADRATIC);
+            String populationField = (String) Params.getValue(input,
+                    KernelDensityProcessFactory.populationField, null);
+            Double searchRadius = (Double) Params.getValue(input,
+                    KernelDensityProcessFactory.searchRadius, 0.0);
+            Double cellSize = (Double) Params.getValue(input, KernelDensityProcessFactory.cellSize,
+                    0.0);
+            ReferencedEnvelope extent = (ReferencedEnvelope) Params.getValue(input,
+                    KernelDensityProcessFactory.extent, null);
+
+            monitor.setTask(Text.text("Processing ..."));
+            monitor.progress(25.0f);
+
+            if (monitor.isCanceled()) {
+                return null; // user has canceled this operation
+            }
+
+            // start process
+            ReferencedEnvelope boundingBox = inputFeatures.getBounds();
+            if (extent != null) {
+                boundingBox = extent;
+            }
+
+            // get default cell size from extent
+            if (cellSize == null || Double.isNaN(cellSize) || cellSize == 0.0) {
+                cellSize = Math.min(boundingBox.getWidth(), boundingBox.getHeight()) / 250.0;
+                LOGGER.warning("default cell size = " + cellSize);
+            }
+
+            if (searchRadius == null || Double.isNaN(searchRadius) || searchRadius == 0) {
+                searchRadius = Math.min(boundingBox.getWidth(), boundingBox.getHeight()) / 30.0;
+                LOGGER.warning("default neighborhood = Circle + Radius(" + searchRadius + ")");
+            }
+            
+            String version = JAI.getBuildVersion();
+            LOGGER.warning(version);
+
+            GridCoverage2D resultGc = null;
+            RasterKernelDensityOperation process = new RasterKernelDensityOperation();
+            process.getRasterEnvironment().setExtent(boundingBox);
+            process.getRasterEnvironment().setCellSize(cellSize);
+            process.setKernelType(kernelType);
+            resultGc = process.execute(inputFeatures, populationField, searchRadius);
+            // end process
+
+            monitor.setTask(Text.text("Encoding result"));
+            monitor.progress(90.0f);
+
+            Map<String, Object> resultMap = new HashMap<String, Object>();
+            resultMap.put(KernelDensityProcessFactory.RESULT.key, resultGc);
+            monitor.complete(); // same as 100.0f
+
+            return resultMap;
+        } catch (Exception eek) {
+            monitor.exceptionOccurred(eek);
+            return null;
+        } finally {
+            monitor.dispose();
+        }
+    }
+
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/KernelDensityProcessFactory.java
@@ -0,0 +1,137 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.Parameter;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.NameImpl;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.process.Process;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterKernelDensityOperation.KernelType;
+import org.geotools.util.KVP;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.InternationalString;
+
+/**
+ * KernelDensityProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class KernelDensityProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging.getLogger(KernelDensityProcessFactory.class);
+
+    private static final String PROCESS_NAME = "KernelDensity";
+
+    /*
+     * KernelDensity(SimpleFeatureCollection inputFeatures, KernelType kernelType, String populationField, Double searchRadius, Double cellSize,
+     * ReferencedEnvelope extent): GridCoverage2D
+     */
+
+    public KernelDensityProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new KernelDensityProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("KernelDensity.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("KernelDensity.description");
+    }
+
+    /** inputFeatures */
+    public static final Parameter<SimpleFeatureCollection> inputFeatures = new Parameter<SimpleFeatureCollection>(
+            "inputFeatures", SimpleFeatureCollection.class,
+            getResource("KernelDensity.inputFeatures.title"),
+            getResource("KernelDensity.inputFeatures.description"), true, 1, 1, null, new KVP(
+                    Parameter.FEATURE_TYPE, "Point"));
+
+    /** kernelType */
+    public static final Parameter<KernelType> kernelType = new Parameter<KernelType>("kernelType",
+            KernelType.class, getResource("KernelDensity.kernelType.title"),
+            getResource("KernelDensity.kernelType.description"), false, 0, 1, KernelType.QUADRATIC,
+            null);
+
+    /** populationField */
+    public static final Parameter<String> populationField = new Parameter<String>(
+            "populationField", String.class, getResource("KernelDensity.populationField.title"),
+            getResource("KernelDensity.populationField.description"), false, 0, 1, null, new KVP(
+                    Parameter.OPTIONS, "inputFeatures.Number"));
+
+    /** searchRadius */
+    public static final Parameter<Double> searchRadius = new Parameter<Double>("searchRadius",
+            Double.class, getResource("KernelDensity.searchRadius.title"),
+            getResource("KernelDensity.searchRadius.description"), false, 0, 1,
+            Double.valueOf(0.0), null);
+
+    /** cellSize */
+    public static final Parameter<Double> cellSize = new Parameter<Double>("cellSize",
+            Double.class, getResource("KernelDensity.cellSize.title"),
+            getResource("KernelDensity.cellSize.description"), false, 0, 1, Double.valueOf(0.0),
+            null);
+
+    /** extent */
+    public static final Parameter<ReferencedEnvelope> extent = new Parameter<ReferencedEnvelope>(
+            "extent", ReferencedEnvelope.class, getResource("KernelDensity.extent.title"),
+            getResource("KernelDensity.extent.description"), false, 0, 1, null, null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputFeatures.key, inputFeatures);
+        parameterInfo.put(kernelType.key, kernelType);
+        parameterInfo.put(populationField.key, populationField);
+        parameterInfo.put(searchRadius.key, searchRadius);
+        parameterInfo.put(cellSize.key, cellSize);
+        parameterInfo.put(extent.key, extent);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<GridCoverage2D> RESULT = new Parameter<GridCoverage2D>("result",
+            GridCoverage2D.class, getResource("KernelDensity.result.title"),
+            getResource("KernelDensity.result.description"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}

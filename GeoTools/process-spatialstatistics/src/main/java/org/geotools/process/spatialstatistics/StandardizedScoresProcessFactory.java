c48ea5cb93a443f04a8a083db1b8e095cbf26dba
Minpa Lee <mapplus@gmail.com>
2015-06-05 16:17:49 +0900

    added standardized scores process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/StandardizedScoresProcessFactory.java
@@ -0,0 +1,123 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.data.Parameter;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.KVP;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.InternationalString;
+
+/**
+ * StandardizedScoresProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class StandardizedScoresProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging
+            .getLogger(StandardizedScoresProcessFactory.class);
+
+    private static final String PROCESS_NAME = "StandardizedScores";
+
+    /*
+     * StandardizedScores(SimpleFeatureCollection inputFeatures, String xField, String yField, String targetField): SimpleFeatureCollection
+     */
+
+    public StandardizedScoresProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new StandardizedScoresProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("StandardizedScores.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("StandardizedScores.description");
+    }
+
+    /** inputFeatures */
+    public static final Parameter<SimpleFeatureCollection> inputFeatures = new Parameter<SimpleFeatureCollection>(
+            "inputFeatures", SimpleFeatureCollection.class,
+            getResource("StandardizedScores.inputFeatures.title"),
+            getResource("StandardizedScores.inputFeatures.description"), true, 1, 1, null, new KVP(
+                    Parameter.FEATURE_TYPE, "All"));
+
+    /** xField */
+    public static final Parameter<String> xField = new Parameter<String>("xField", String.class,
+            getResource("StandardizedScores.xField.title"),
+            getResource("StandardizedScores.xField.description"), true, 1, 1, null, new KVP(
+                    Parameter.OPTIONS, "inputFeatures.Number"));
+
+    /** yField */
+    public static final Parameter<String> yField = new Parameter<String>("yField", String.class,
+            getResource("StandardizedScores.yField.title"),
+            getResource("StandardizedScores.yField.description"), true, 1, 1, null, new KVP(
+                    Parameter.OPTIONS, "inputFeatures.Number"));
+
+    /** targetField */
+    public static final Parameter<String> targetField = new Parameter<String>("targetField",
+            String.class, getResource("StandardizedScores.targetField.title"),
+            getResource("StandardizedScores.targetField.description"), false, 0, 1, "std_scr",
+            new KVP(Parameter.OPTIONS, "inputFeatures.Number"));
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputFeatures.key, inputFeatures);
+        parameterInfo.put(xField.key, xField);
+        parameterInfo.put(yField.key, yField);
+        parameterInfo.put(targetField.key, targetField);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<SimpleFeatureCollection> RESULT = new Parameter<SimpleFeatureCollection>(
+            "result", SimpleFeatureCollection.class,
+            getResource("StandardizedScores.result.title"),
+            getResource("StandardizedScores.result.description"), true, 1, 1, null, new KVP(
+                    Parameter.OPTIONS, "Quantile.targetField"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/pattern/StandardizedScoresOperation.java
@@ -0,0 +1,137 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.pattern;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.process.spatialstatistics.core.FeatureTypes;
+import org.geotools.process.spatialstatistics.core.SpatialEvent;
+import org.geotools.process.spatialstatistics.operations.GeneralOperation;
+import org.geotools.process.spatialstatistics.storage.IFeatureInserter;
+import org.geotools.util.logging.Logging;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.expression.Expression;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * Calculates a standardized scores.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class StandardizedScoresOperation extends GeneralOperation {
+    protected static final Logger LOGGER = Logging.getLogger(StandardizedScoresOperation.class);
+
+    private int count = 0;
+
+    private double X = 0;
+
+    private double Y = 0;
+
+    private List<SpatialEvent> events = new ArrayList<SpatialEvent>();
+
+    private void preCalculate(SimpleFeatureCollection features, Expression xExpression,
+            Expression yExpression) {
+        int oid = 0;
+        SimpleFeatureIterator featureIter = null;
+        try {
+            featureIter = features.features();
+            while (featureIter.hasNext()) {
+                SimpleFeature feature = featureIter.next();
+                Geometry geometry = (Geometry) feature.getDefaultGeometry();
+                Coordinate coordinate = geometry.getCentroid().getCoordinate();
+
+                Double xVal = xExpression.evaluate(feature, Double.class);
+                Double yVal = yExpression.evaluate(feature, Double.class);
+                if (xVal == null || yVal == null) {
+                    continue;
+                }
+
+                count += 1;
+                X += xVal;
+                Y += yVal;
+
+                events.add(new SpatialEvent(oid++, coordinate, xVal, yVal));
+            }
+        } finally {
+            featureIter.close();
+        }
+    }
+
+    public SimpleFeatureCollection execute(SimpleFeatureCollection features, String xField,
+            String yField, String targetField) throws IOException {
+        String typeName = features.getSchema().getTypeName();
+        SimpleFeatureType schema = FeatureTypes.build(features.getSchema(), typeName);
+        schema = FeatureTypes.add(schema, targetField, Double.class);
+
+        // 1. pre calculation
+        xField = FeatureTypes.validateProperty(features.getSchema(), xField);
+        yField = FeatureTypes.validateProperty(features.getSchema(), yField);
+        Expression xExpression = ff.property(xField);
+        Expression yExpression = ff.property(yField);
+
+        preCalculate(features, xExpression, yExpression);
+
+        // 2. calculate standardized scores
+        IFeatureInserter featureWriter = getFeatureWriter(schema);
+        SimpleFeatureIterator featureIter = features.features();
+        try {
+            while (featureIter.hasNext()) {
+                SimpleFeature feature = featureIter.next();
+                Double xVal = xExpression.evaluate(feature, Double.class);
+                Double yVal = yExpression.evaluate(feature, Double.class);
+
+                Double stdscr = null;
+                if (xVal != null && yVal != null) {
+                    double XX = xVal / X;
+                    double YY = yVal / Y;
+
+                    double dZSum = 0.0d;
+                    for (SpatialEvent curE : events) {
+                        double xx = curE.weight / X;
+                        double yy = curE.population / Y;
+                        double dZ = Math.pow(xx - yy, 2) / count;
+                        dZSum += dZ;
+                    }
+                    stdscr = (XX - YY) / Math.sqrt(dZSum);
+                }
+
+                // create & insert feature
+                SimpleFeature newFeature = featureWriter.buildFeature(feature.getID());
+                featureWriter.copyAttributes(feature, newFeature, true);
+                newFeature.setAttribute(targetField, stdscr);
+                featureWriter.write(newFeature);
+            }
+        } catch (Exception e) {
+            featureWriter.rollback(e);
+        } finally {
+            featureWriter.close();
+        }
+
+        return featureWriter.getFeatureCollection();
+    }
+
+}

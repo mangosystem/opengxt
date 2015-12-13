a8a974a33c96d8d7488d2dcb3457b357b9151607
Minpa Lee <mapplus@gmail.com>
2015-12-13 19:53:33 +0900

    added singlepart to multipart operation

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/SinglepartToMultipartProcessFactory.java
@@ -0,0 +1,114 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.data.Parameter;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.KVP;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.InternationalString;
+
+/**
+ * SinglepartToMultipartProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class SinglepartToMultipartProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging
+            .getLogger(SinglepartToMultipartProcessFactory.class);
+
+    private static final String PROCESS_NAME = "SinglepartToMultipart";
+
+    /*
+     * SinglepartToMultipart(SimpleFeatureCollection inputFeatures, String caseField, Boolean dissolve): SimpleFeatureCollection
+     */
+
+    public SinglepartToMultipartProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new SinglepartToMultipartProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("SinglepartToMultipart.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("SinglepartToMultipart.description");
+    }
+
+    /** inputFeatures */
+    public static final Parameter<SimpleFeatureCollection> inputFeatures = new Parameter<SimpleFeatureCollection>(
+            "inputFeatures", SimpleFeatureCollection.class,
+            getResource("SinglepartToMultipart.inputFeatures.title"),
+            getResource("SinglepartToMultipart.inputFeatures.description"), true, 1, 1, null, null);
+
+    /** caseField */
+    public static final Parameter<String> caseField = new Parameter<String>("caseField",
+            String.class, getResource("SinglepartToMultipart.caseField.title"),
+            getResource("SinglepartToMultipart.caseField.description"), true, 1, 1, null, new KVP(
+                    Parameter.OPTIONS, "inputFeatures.All"));
+
+    /** dissolve */
+    public static final Parameter<Boolean> dissolve = new Parameter<Boolean>("dissolve",
+            Boolean.class, getResource("SinglepartToMultipart.dissolve.title"),
+            getResource("SinglepartToMultipart.dissolve.description"), false, 0, 1, Boolean.FALSE,
+            null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputFeatures.key, inputFeatures);
+        parameterInfo.put(caseField.key, caseField);
+        parameterInfo.put(dissolve.key, dissolve);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<SimpleFeatureCollection> RESULT = new Parameter<SimpleFeatureCollection>(
+            "result", SimpleFeatureCollection.class,
+            getResource("SinglepartToMultipart.result.title"),
+            getResource("SinglepartToMultipart.result.description"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/transformation/MultipartFeatureCollection.java
@@ -0,0 +1,152 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.transformation;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.process.spatialstatistics.core.FeatureTypes;
+import org.geotools.util.logging.Logging;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.expression.Expression;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.operation.union.CascadedPolygonUnion;
+
+/**
+ * Singlepart to Multipart SimpleFeatureCollection Implementation
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class MultipartFeatureCollection extends GXTSimpleFeatureCollection {
+    protected static final Logger LOGGER = Logging.getLogger(MultipartFeatureCollection.class);
+
+    private boolean dissolve;
+
+    private String caseField;
+
+    private SimpleFeatureType schema;
+
+    private Hashtable<Object, List<SimpleFeature>> map;
+
+    public MultipartFeatureCollection(SimpleFeatureCollection delegate, String caseField,
+            boolean dissolve) {
+        super(delegate);
+
+        if (caseField == null || caseField.isEmpty()) {
+            throw new NullPointerException("caseField is null");
+        }
+
+        this.caseField = FeatureTypes.validateProperty(delegate.getSchema(), caseField);
+        this.dissolve = dissolve;
+        this.schema = FeatureTypes.build(delegate, delegate.getSchema().getTypeName());
+        this.init();
+    }
+
+    private void init() {
+        map = new Hashtable<Object, List<SimpleFeature>>();
+
+        SimpleFeatureBuilder builder = new SimpleFeatureBuilder(schema);
+        Expression expression = ff.property(caseField);
+        SimpleFeatureIterator featureIter = delegate.features();
+        try {
+            while (featureIter.hasNext()) {
+                SimpleFeature feature = featureIter.next();
+
+                // create new feature
+                SimpleFeature newFeature = builder.buildFeature(feature.getID());
+                transferAttribute(feature, newFeature);
+
+                Object value = expression.evaluate(feature);
+                if (!map.containsKey(value)) {
+                    map.put(value, new ArrayList<SimpleFeature>());
+                }
+                map.get(value).add(newFeature);
+            }
+        } finally {
+            featureIter.close();
+        }
+    }
+
+    @Override
+    public SimpleFeatureIterator features() {
+        return new MultipartFeatureIterator(map, dissolve);
+    }
+
+    @Override
+    public SimpleFeatureType getSchema() {
+        return schema;
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    static class MultipartFeatureIterator implements SimpleFeatureIterator {
+        private Iterator<Entry<Object, List<SimpleFeature>>> featureIter;
+
+        private boolean dissolve = false;
+
+        public MultipartFeatureIterator(Hashtable<Object, List<SimpleFeature>> map, boolean dissolve) {
+            this.featureIter = map.entrySet().iterator();
+            this.dissolve = dissolve;
+        }
+
+        public void close() {
+            // nothing to do
+        }
+
+        public boolean hasNext() {
+            return featureIter.hasNext();
+        }
+
+        public SimpleFeature next() throws NoSuchElementException {
+            Entry<Object, List<SimpleFeature>> entry = featureIter.next();
+            List<SimpleFeature> featureList = entry.getValue();
+
+            SimpleFeature nextFeature = featureList.get(0);
+
+            List<Geometry> geometries = new ArrayList<Geometry>();
+            for (SimpleFeature feature : featureList) {
+                Geometry geometry = (Geometry) feature.getDefaultGeometry();
+                geometries.add(geometry);
+            }
+
+            if (dissolve) {
+                CascadedPolygonUnion unionOp = new CascadedPolygonUnion(geometries);
+                nextFeature.setDefaultGeometry(unionOp.union());
+            } else {
+                nextFeature.setDefaultGeometry(geometries.get(0).getFactory()
+                        .buildGeometry(geometries));
+            }
+
+            return nextFeature;
+        }
+    }
+}

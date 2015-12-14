8ca269b706200e9cbea40d9970a2846ce42bac29
Minpa Lee <mapplus@gmail.com>
2015-12-15 07:21:47 +0900

    added points to line operation

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/PointsToLineProcessFactory.java
@@ -0,0 +1,119 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.data.Parameter;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.KVP;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.InternationalString;
+
+/**
+ * PointsToLineProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class PointsToLineProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging.getLogger(PointsToLineProcessFactory.class);
+
+    private static final String PROCESS_NAME = "PointsToLine";
+
+    /*
+     * PointsToLine(SimpleFeatureCollection inputFeatures, String lineField, String sortField, Boolean closeLine): SimpleFeatureCollection
+     */
+
+    public PointsToLineProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new PointsToLineProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("PointsToLine.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("PointsToLine.description");
+    }
+
+    /** inputFeatures */
+    public static final Parameter<SimpleFeatureCollection> inputFeatures = new Parameter<SimpleFeatureCollection>(
+            "inputFeatures", SimpleFeatureCollection.class,
+            getResource("PointsToLine.inputFeatures.title"),
+            getResource("PointsToLine.inputFeatures.description"), true, 1, 1, null, new KVP(
+                    Parameter.FEATURE_TYPE, "Point"));
+
+    /** lineField */
+    public static final Parameter<String> lineField = new Parameter<String>("lineField",
+            String.class, getResource("PointsToLine.lineField.title"),
+            getResource("PointsToLine.lineField.description"), false, 0, 1, null, new KVP(
+                    Parameter.OPTIONS, "inputFeatures.All"));
+
+    /** sortField */
+    public static final Parameter<String> sortField = new Parameter<String>("sortField",
+            String.class, getResource("PointsToLine.sortField.title"),
+            getResource("PointsToLine.sortField.description"), false, 0, 1, null, new KVP(
+                    Parameter.OPTIONS, "inputFeatures.All"));
+
+    /** closeLine */
+    public static final Parameter<Boolean> closeLine = new Parameter<Boolean>("closeLine",
+            Boolean.class, getResource("PointsToLine.closeLine.title"),
+            getResource("PointsToLine.closeLine.description"), false, 0, 1, Boolean.FALSE, null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputFeatures.key, inputFeatures);
+        parameterInfo.put(lineField.key, lineField);
+        parameterInfo.put(sortField.key, sortField);
+        parameterInfo.put(closeLine.key, closeLine);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<SimpleFeatureCollection> RESULT = new Parameter<SimpleFeatureCollection>(
+            "result", SimpleFeatureCollection.class, getResource("PointsToLine.result.title"),
+            getResource("PointsToLine.result.description"), true, 1, 1, null, null);
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/operations/PointsToLineOperation.java
@@ -0,0 +1,188 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.operations;
+
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.logging.Logger;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.process.spatialstatistics.core.FeatureTypes;
+import org.geotools.process.spatialstatistics.storage.IFeatureInserter;
+import org.geotools.util.logging.Logging;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+import org.opengis.filter.expression.Expression;
+import org.opengis.filter.sort.SortBy;
+import org.opengis.filter.sort.SortOrder;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import com.vividsolutions.jts.geom.CoordinateList;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.Polygon;
+
+/**
+ * Creates line features from points.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class PointsToLineOperation extends GeneralOperation {
+    protected static final Logger LOGGER = Logging.getLogger(PointsToLineOperation.class);
+
+    private static final String length = "length";
+
+    private boolean closeLine = false;
+
+    private String lineField = null;
+
+    public SimpleFeatureCollection execute(SimpleFeatureCollection inputFeatures, String lineField,
+            String sortField, boolean closeLine) throws IOException {
+
+        this.closeLine = closeLine;
+
+        // prepare feature type
+        SimpleFeatureType inputSchema = inputFeatures.getSchema();
+        CoordinateReferenceSystem crs = inputSchema.getCoordinateReferenceSystem();
+        String geomName = inputFeatures.getSchema().getGeometryDescriptor().getLocalName();
+        SimpleFeatureType featureType = FeatureTypes.getDefaultType(getOutputTypeName(), geomName,
+                LineString.class, crs);
+
+        if (closeLine) {
+            featureType = FeatureTypes.getDefaultType(getOutputTypeName(), geomName, Polygon.class,
+                    crs);
+        }
+
+        boolean hasLineField = lineField != null && lineField.length() > 0;
+        boolean hasSortField = sortField != null && sortField.length() > 0;
+
+        if (hasLineField) {
+            lineField = FeatureTypes.validateProperty(inputSchema, lineField);
+            hasLineField = inputSchema.indexOf(lineField) != -1;
+            if (hasLineField) {
+                featureType = FeatureTypes.add(featureType, inputSchema.getDescriptor(lineField));
+                this.lineField = lineField;
+            }
+        }
+        featureType = FeatureTypes.add(featureType, length, Double.class, 33);
+
+        if (hasSortField) {
+            sortField = FeatureTypes.validateProperty(inputSchema, sortField);
+            hasSortField = inputSchema.indexOf(sortField) != -1;
+        }
+
+        // prepare transactional feature store
+        IFeatureInserter featureWriter = getFeatureWriter(featureType);
+        try {
+            SimpleFeatureCollection subCollection = inputFeatures;
+            if (hasLineField) {
+                Hashtable<Object, Object> uvValues = getUniqueValues(inputFeatures, lineField);
+                for (Object uvValue : uvValues.keySet()) {
+                    Filter filter = ff.equal(ff.property(lineField), ff.literal(uvValue), true);
+                    subCollection = inputFeatures.subCollection(filter);
+                    if (hasSortField) {
+                        SortBy sort = ff.sort(sortField, SortOrder.ASCENDING);
+                        insertFeatures(subCollection.sort(sort), uvValue, featureWriter);
+                    } else {
+                        insertFeatures(subCollection, uvValue, featureWriter);
+                    }
+                }
+            } else {
+                if (hasSortField) {
+                    subCollection = inputFeatures.sort(ff.sort(sortField, SortOrder.ASCENDING));
+                }
+                insertFeatures(subCollection, null, featureWriter);
+            }
+        } catch (Exception e) {
+            featureWriter.rollback(e);
+        } finally {
+            featureWriter.close(null);
+        }
+
+        return featureWriter.getFeatureCollection();
+    }
+
+    private void insertFeatures(SimpleFeatureCollection inputFeatures, Object lineValue,
+            IFeatureInserter featureWriter) throws IOException {
+        CoordinateList coordinates = new CoordinateList();
+
+        SimpleFeatureIterator featureIter = inputFeatures.features();
+        try {
+            while (featureIter.hasNext()) {
+                SimpleFeature feature = featureIter.next();
+                Geometry geometry = (Geometry) feature.getDefaultGeometry();
+                if (geometry == null || geometry.isEmpty()) {
+                    continue;
+                }
+                coordinates.add(geometry.getCoordinate(), false);
+            }
+        } finally {
+            featureIter.close();
+        }
+
+        if (coordinates.size() <= 1) {
+            return;
+        }
+
+        if (coordinates.size() > 1) {
+            // create feature and set geometry
+            Geometry geometry = null;
+            if (closeLine) {
+                coordinates.add(coordinates.get(0), false);
+                geometry = gf.createPolygon(coordinates.toCoordinateArray());
+            } else {
+                geometry = gf.createLineString(coordinates.toCoordinateArray());
+            }
+
+            if (geometry == null || geometry.isEmpty()) {
+                return;
+            }
+
+            SimpleFeature newFeature = featureWriter.buildFeature();
+            newFeature.setDefaultGeometry(geometry);
+            if (lineValue != null && lineField != null) {
+                newFeature.setAttribute(lineField, lineValue);
+            }
+            newFeature.setAttribute(length, geometry.getLength());
+            featureWriter.write(newFeature);
+        }
+    }
+
+    private Hashtable<Object, Object> getUniqueValues(SimpleFeatureCollection inputFeatures,
+            String lineField) {
+        Hashtable<Object, Object> table = new Hashtable<Object, Object>();
+        SimpleFeatureIterator featureIter = inputFeatures.features();
+        try {
+            Expression expression = ff.property(lineField);
+            while (featureIter.hasNext()) {
+                Object value = expression.evaluate(featureIter.next());
+                if (value == null) {
+                    // skip null value feature
+                    continue;
+                }
+                table.put(value, value);
+            }
+        } finally {
+            featureIter.close();
+        }
+        return table;
+    }
+}

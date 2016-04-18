2da99418e936f29dc661779d59987e3f8d3184e3
Minpa Lee <mapplus@gmail.com>
2016-04-18 16:29:58 +0900

    added Quadrat Analysis PPIO

---
+++ GeoServer/gs-wps-spatialstatistics/src/main/java/org/geoserver/wps/spatialstatistics/ppio/QuadratResultPPIO.java
@@ -0,0 +1,73 @@
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2014 MangoSystem
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wps.spatialstatistics.ppio;
+
+import java.io.InputStream;
+
+import javax.xml.namespace.QName;
+
+import org.geoserver.wps.ppio.XStreamPPIO;
+import org.geotools.process.spatialstatistics.pattern.QuadratOperation.QuadratResult;
+import org.xml.sax.ContentHandler;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+import com.thoughtworks.xstream.io.xml.SaxWriter;
+import com.thoughtworks.xstream.io.xml.XmlFriendlyNameCoder;
+import com.thoughtworks.xstream.mapper.MapperWrapper;
+
+/**
+ * A PPIO to generate good looking xml for the Quadrat analysis process results
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class QuadratResultPPIO extends XStreamPPIO {
+
+    final XmlFriendlyNameCoder nameCoder = new XmlFriendlyNameCoder("__", "_");
+
+    static final QName PPIO_NAME = new QName("http://www.opengis.net/statistics", "QuadratAnalysis");
+
+    protected QuadratResultPPIO() {
+        super(QuadratResult.class, PPIO_NAME);
+    }
+
+    @Override
+    protected XStream buildXStream() {
+        XStream xstream = new XStream(new DomDriver("UTF-8", nameCoder)) {
+            @Override
+            protected boolean useXStream11XmlFriendlyMapper() {
+                return true;
+            }
+
+            @Override
+            protected MapperWrapper wrapMapper(MapperWrapper next) {
+                return new UppercaseTagMapper(next);
+            };
+        };
+
+        xstream.processAnnotations(QuadratResult.class);
+        xstream.alias("QuadratAnalysis", QuadratResult.class);
+
+        return xstream;
+    }
+
+    @Override
+    public void encode(Object object, ContentHandler handler) throws Exception {
+        // bind with the content handler
+        SaxWriter writer = new SaxWriter(nameCoder);
+        writer.setContentHandler(handler);
+
+        // write out xml
+        buildXStream().marshal(object, writer);
+    }
+
+    @Override
+    public Object decode(InputStream input) throws Exception {
+        return buildXStream().fromXML(input);
+    }
+}

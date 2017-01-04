057f067cc5499109800a050f439bae71f95fd288
Minpa Lee <mapplus@gmail.com>
2017-01-05 00:08:27 +0900

    added convexhull process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/FeatureToConvexHullProcess.java
@@ -0,0 +1,94 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.data.DataUtilities;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.transformation.ConvexHullFeatureCollection;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Creates a polygon features, each of which represents the convexhull polygon of an input feature.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class FeatureToConvexHullProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(FeatureToConvexHullProcess.class);
+
+    public FeatureToConvexHullProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static SimpleFeatureCollection process(SimpleFeatureCollection inputFeatures,
+            Boolean singlePart, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(FeatureToConvexHullProcessFactory.inputFeatures.key, inputFeatures);
+        map.put(FeatureToConvexHullProcessFactory.singlePart.key, singlePart);
+
+        Process process = new FeatureToConvexHullProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (SimpleFeatureCollection) resultMap
+                    .get(FeatureToConvexHullProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        SimpleFeatureCollection inputFeatures = (SimpleFeatureCollection) Params.getValue(input,
+                FeatureToConvexHullProcessFactory.inputFeatures, null);
+        if (inputFeatures == null) {
+            throw new NullPointerException("inputFeatures parameter required");
+        }
+
+        Boolean singlePart = (Boolean) Params.getValue(input,
+                FeatureToConvexHullProcessFactory.singlePart,
+                FeatureToConvexHullProcessFactory.singlePart.sample);
+
+        // start process
+        SimpleFeatureCollection resultFc = new ConvexHullFeatureCollection(inputFeatures,
+                singlePart);
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(FeatureToConvexHullProcessFactory.RESULT.key, DataUtilities.simple(resultFc));
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/transformation/ConvexHullFeatureCollection.java
@@ -0,0 +1,128 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.transformation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.feature.collection.SubFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.filter.function.StaticGeometry;
+import org.geotools.process.spatialstatistics.core.FeatureTypes;
+import org.geotools.util.logging.Logging;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Polygon;
+
+/**
+ * Feature To ConvexHull SimpleFeatureCollection Implementation
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class ConvexHullFeatureCollection extends GXTSimpleFeatureCollection {
+    protected static final Logger LOGGER = Logging.getLogger(ConvexHullFeatureCollection.class);
+
+    private boolean singlePart;
+
+    private SimpleFeatureType schema;
+
+    public ConvexHullFeatureCollection(SimpleFeatureCollection delegate, boolean singlePart) {
+        super(delegate);
+
+        this.singlePart = singlePart;
+
+        String typeName = delegate.getSchema().getTypeName();
+        this.schema = FeatureTypes.build(delegate.getSchema(), typeName, Polygon.class);
+    }
+
+    @Override
+    public SimpleFeatureIterator features() {
+        return new ConvexHullFeatureIterator(delegate.features(), getSchema(), singlePart);
+    }
+
+    @Override
+    public SimpleFeatureType getSchema() {
+        return schema;
+    }
+
+    @Override
+    public SimpleFeatureCollection subCollection(Filter filter) {
+        if (filter == Filter.INCLUDE) {
+            return this;
+        }
+        return new SubFeatureCollection(this, filter);
+    }
+
+    static class ConvexHullFeatureIterator implements SimpleFeatureIterator {
+        private SimpleFeatureIterator delegate;
+
+        private boolean singlePart = false;
+
+        private SimpleFeatureBuilder builder;
+
+        public ConvexHullFeatureIterator(SimpleFeatureIterator delegate, SimpleFeatureType schema,
+                boolean singlePart) {
+            this.delegate = delegate;
+
+            this.singlePart = singlePart;
+            this.builder = new SimpleFeatureBuilder(schema);
+        }
+
+        public void close() {
+            delegate.close();
+        }
+
+        public boolean hasNext() {
+            return delegate.hasNext();
+        }
+
+        public SimpleFeature next() throws NoSuchElementException {
+            SimpleFeature sourceFeature = delegate.next();
+
+            for (Object attribute : sourceFeature.getAttributes()) {
+                if (attribute instanceof Geometry) {
+                    Geometry geometry = (Geometry) attribute;
+                    if (!singlePart && geometry.getNumGeometries() > 1) {
+                        List<Geometry> geomList = new ArrayList<Geometry>();
+                        for (int index = 0; index < geometry.getNumGeometries(); index++) {
+                            Geometry part = geometry.getGeometryN(index);
+                            geomList.add(StaticGeometry.convexHull(part));
+                        }
+                        attribute = geometry.getFactory().buildGeometry(geomList);
+                    } else {
+                        attribute = StaticGeometry.convexHull(geometry);
+                    }
+                }
+                builder.add(attribute);
+            }
+
+            SimpleFeature nextFeature = builder.buildFeature(sourceFeature.getID());
+            builder.reset();
+
+            return nextFeature;
+        }
+    }
+}

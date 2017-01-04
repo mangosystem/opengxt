0f14633b5bbf3cbdd05cab44c7f5514df707964d
Minpa Lee <mapplus@gmail.com>
2017-01-05 00:09:48 +0900

    added octagonal envelope process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/FeatureToOctagonalEnvelopeProcess.java
@@ -0,0 +1,96 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.data.DataUtilities;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.transformation.OctagonalEnvelopeFeatureCollection;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Creates a polygon features, each of which represents the minimum rectangle of an input feature.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class FeatureToOctagonalEnvelopeProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging
+            .getLogger(FeatureToOctagonalEnvelopeProcess.class);
+
+    public FeatureToOctagonalEnvelopeProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static SimpleFeatureCollection process(SimpleFeatureCollection inputFeatures,
+            Boolean singlePart, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(FeatureToOctagonalEnvelopeProcessFactory.inputFeatures.key, inputFeatures);
+        map.put(FeatureToOctagonalEnvelopeProcessFactory.singlePart.key, singlePart);
+
+        Process process = new FeatureToOctagonalEnvelopeProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (SimpleFeatureCollection) resultMap
+                    .get(FeatureToOctagonalEnvelopeProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        SimpleFeatureCollection inputFeatures = (SimpleFeatureCollection) Params.getValue(input,
+                FeatureToOctagonalEnvelopeProcessFactory.inputFeatures, null);
+        if (inputFeatures == null) {
+            throw new NullPointerException("inputFeatures parameter required");
+        }
+
+        Boolean singlePart = (Boolean) Params.getValue(input,
+                FeatureToOctagonalEnvelopeProcessFactory.singlePart,
+                FeatureToOctagonalEnvelopeProcessFactory.singlePart.sample);
+
+        // start process
+        SimpleFeatureCollection resultFc = new OctagonalEnvelopeFeatureCollection(inputFeatures,
+                singlePart);
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(FeatureToOctagonalEnvelopeProcessFactory.RESULT.key,
+                DataUtilities.simple(resultFc));
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/FeatureToOctagonalEnvelopeProcessFactory.java
@@ -0,0 +1,107 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.data.Parameter;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.InternationalString;
+
+/**
+ * FeatureToOctagonalEnvelopeProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class FeatureToOctagonalEnvelopeProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging
+            .getLogger(FeatureToOctagonalEnvelopeProcessFactory.class);
+
+    private static final String PROCESS_NAME = "FeatureToOctagonalEnvelope";
+
+    /*
+     * FeatureToOctagonalEnvelope(SimpleFeatureCollection inputFeatures, Boolean singlePart): SimpleFeatureCollection
+     */
+
+    public FeatureToOctagonalEnvelopeProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new FeatureToOctagonalEnvelopeProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("FeatureToOctagonalEnvelope.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("FeatureToOctagonalEnvelope.description");
+    }
+
+    /** inputFeatures */
+    public static final Parameter<SimpleFeatureCollection> inputFeatures = new Parameter<SimpleFeatureCollection>(
+            "inputFeatures", SimpleFeatureCollection.class,
+            getResource("FeatureToOctagonalEnvelope.inputFeatures.title"),
+            getResource("FeatureToOctagonalEnvelope.inputFeatures.description"), true, 1, 1, null,
+            null);
+
+    /** singlePart */
+    public static final Parameter<Boolean> singlePart = new Parameter<Boolean>("singlePart",
+            Boolean.class, getResource("FeatureToOctagonalEnvelope.singlePart.title"),
+            getResource("FeatureToOctagonalEnvelope.singlePart.description"), false, 0, 1,
+            Boolean.TRUE, null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputFeatures.key, inputFeatures);
+        parameterInfo.put(singlePart.key, singlePart);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<SimpleFeatureCollection> RESULT = new Parameter<SimpleFeatureCollection>(
+            "result", SimpleFeatureCollection.class,
+            getResource("FeatureToOctagonalEnvelope.result.title"),
+            getResource("FeatureToOctagonalEnvelope.result.description"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}

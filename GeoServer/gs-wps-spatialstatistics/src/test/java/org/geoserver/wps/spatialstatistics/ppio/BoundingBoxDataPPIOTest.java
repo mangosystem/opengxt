237e3032cb26a05d4cb59eba865a5359a74e86b0
Minpa Lee <mapplus@gmail.com>
2017-02-03 01:52:24 +0900

    added boundingboxdata & expression ppio testcase

---
+++ GeoServer/gs-wps-spatialstatistics/src/test/java/org/geoserver/wps/spatialstatistics/ppio/BoundingBoxDataPPIOTest.java
@@ -0,0 +1,34 @@
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2014 MangoSystem
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wps.spatialstatistics.ppio;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.junit.Test;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+public class BoundingBoxDataPPIOTest {
+
+    @Test
+    public void test() throws Exception {
+        BoundingBoxDataPPIO ppio = new BoundingBoxDataPPIO();
+
+        CoordinateReferenceSystem crs = null;
+        ReferencedEnvelope envelope = new ReferencedEnvelope(1.0, 3.0, 1.0, 3.0, crs);
+
+        // encode
+        String encoded = ppio.encode(envelope);
+        assertNotNull(encoded);
+
+        // decode
+        ReferencedEnvelope decoded = (ReferencedEnvelope) ppio.decode(encoded);
+        assertNotNull(decoded);
+
+        assertEquals(envelope, decoded);
+    }
+}
---
+++ GeoServer/gs-wps-spatialstatistics/src/test/java/org/geoserver/wps/spatialstatistics/ppio/ExpressionPPIOTest.java
@@ -0,0 +1,65 @@
+/* (c) 2014 Open Source Geospatial Foundation - all rights reserved
+ * (c) 2014 MangoSystem
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.wps.spatialstatistics.ppio;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.filter.text.ecql.ECQL;
+import org.junit.Before;
+import org.junit.Test;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.expression.Expression;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.WKTReader;
+
+public class ExpressionPPIOTest {
+
+    private WKTReader reader = new WKTReader();
+
+    private SimpleFeature feature;
+
+    @Before
+    public void setup() throws Exception {
+        SimpleFeatureTypeBuilder tb = new SimpleFeatureTypeBuilder();
+        tb.add("geom", Polygon.class, "EPSG:4326");
+        tb.add("area", Double.class);
+        tb.setName("circle");
+        SimpleFeatureType ft = tb.buildFeatureType();
+
+        Geometry geom = reader.read("POINT(0 0)").buffer(10);
+
+        SimpleFeatureBuilder fb = new SimpleFeatureBuilder(ft);
+        fb.add(geom);
+        fb.add(geom.getArea());
+        feature = fb.buildFeature(null);
+    }
+
+    @Test
+    public void test() throws Exception {
+        ExpressionPPIO ppio = new ExpressionPPIO();
+
+        String ecqlExpression = "[area] / 100";
+        Expression expression = ECQL.toExpression(ecqlExpression);
+
+        // encode
+        String encoded = ppio.encode(expression);
+        assertNotNull(encoded);
+
+        // decode
+        Expression decoded = (Expression) ppio.decode(encoded);
+        assertEquals(expression, decoded);
+
+        Geometry geom = (Geometry) feature.getDefaultGeometry();
+        assertEquals(decoded.evaluate(feature, Double.class), Double.valueOf(geom.getArea() / 100));
+    }
+
+}

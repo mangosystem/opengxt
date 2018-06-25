662681b56301bc7c8907b8174f9e044bebc9dd99
MapPlus <mapplus@gmail.com>
2018-06-25 15:13:17 +0900

    added raster tri process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterTRIProcess.java
@@ -0,0 +1,88 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterTRIOperation;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.ProgressListener;
+
+/**
+ * TRI - Terrain Ruggedness Index is as described in Wilson et al (2007).<br>
+ * This is based on the method of Valentine et al. (2004). <br>
+ * Terrain Ruggedness Index is average difference in height
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterTRIProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(RasterTRIProcess.class);
+
+    public RasterTRIProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static GridCoverage2D process(GridCoverage2D inputCoverage, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(RasterTRIProcessFactory.inputCoverage.key, inputCoverage);
+
+        Process process = new RasterTRIProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (GridCoverage2D) resultMap.get(RasterTRIProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        GridCoverage2D inputCoverage = (GridCoverage2D) Params.getValue(input,
+                RasterTRIProcessFactory.inputCoverage, null);
+        if (inputCoverage == null) {
+            throw new NullPointerException("inputCoverage parameter required");
+        }
+
+        // start process
+        RasterTRIOperation process = new RasterTRIOperation();
+        GridCoverage2D extractedGC = process.execute(inputCoverage);
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(RasterTRIProcessFactory.RESULT.key, extractedGC);
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/gridcoverage/RasterTRIOperation.java
@@ -0,0 +1,98 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.gridcoverage;
+
+import java.util.logging.Logger;
+
+import javax.media.jai.iterator.RectIterFactory;
+import javax.media.jai.iterator.WritableRectIter;
+
+import org.geotools.coverage.grid.GridCoordinates2D;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.process.spatialstatistics.core.SSUtils;
+import org.geotools.process.spatialstatistics.enumeration.RasterPixelType;
+import org.geotools.util.logging.Logging;
+import org.jaitools.tiledimage.DiskMemImage;
+
+/**
+ * TRI - Terrain Ruggedness Index is as described in Wilson et al (2007).<br>
+ * This is based on the method of Valentine et al. (2004). <br>
+ * Terrain Ruggedness Index is average difference in height
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterTRIOperation extends AbstractSurfaceOperation {
+    protected static final Logger LOGGER = Logging.getLogger(RasterTRIOperation.class);
+
+    public GridCoverage2D execute(GridCoverage2D inputGc) {
+        DiskMemImage outputImage = this.createDiskMemImage(inputGc, RasterPixelType.FLOAT);
+
+        grid2D = inputGc;
+        srcNoData = RasterHelper.getNoDataValue(inputGc);
+        NoData = -9999;
+
+        final java.awt.Rectangle bounds = outputImage.getBounds();
+        WritableRectIter writer = RectIterFactory.createWritable(outputImage, bounds);
+
+        GridCoordinates2D pos = new GridCoordinates2D();
+
+        int y = 0;
+        writer.startLines();
+        while (!writer.finishedLines()) {
+
+            int x = 0;
+            writer.startPixels();
+            while (!writer.finishedPixels()) {
+                pos.setLocation(x, y);
+
+                visitTRI(writer, pos);
+
+                writer.nextPixel();
+                x++;
+            }
+
+            writer.nextLine();
+            y++;
+        }
+
+        return createGridCoverage("TRI", outputImage);
+    }
+
+    private void visitTRI(WritableRectIter writer, GridCoordinates2D pos) {
+        // +-------+ +-------+
+        // | 0 1 2 | | a b c |
+        // | 3 4 5 |>| d e f |
+        // | 6 7 8 | | g h i |
+        // +-------+ +-------+
+        double[][] mx = getSubMatrix(grid2D, pos, 3, 3);
+        if (Double.isNaN(mx[1][1]) || SSUtils.compareDouble(srcNoData, mx[1][1])) {
+            writer.setSample(0, NoData);
+            return;
+        }
+
+        // Terrain Ruggedness Index is average difference in height
+        final double tri = (Math.abs(mx[0][0] - mx[1][1]) + Math.abs(mx[1][0] - mx[1][1])
+                + Math.abs(mx[2][0] - mx[1][1]) + Math.abs(mx[0][1] - mx[1][1])
+                + Math.abs(mx[2][1] - mx[1][1]) + Math.abs(mx[0][2] - mx[1][1])
+                + Math.abs(mx[1][2] - mx[1][1]) + Math.abs(mx[2][2] - mx[1][1])) / 8.0;
+
+        writer.setSample(0, tri);
+        updateStatistics(tri);
+    }
+}

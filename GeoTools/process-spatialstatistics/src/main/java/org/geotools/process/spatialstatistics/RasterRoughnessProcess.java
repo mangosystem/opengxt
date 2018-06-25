b2535a5232bfc1d7e7d76471380735647d4036c8
MapPlus <mapplus@gmail.com>
2018-06-25 15:12:45 +0900

    added raster roughness process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterRoughnessProcess.java
@@ -0,0 +1,87 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterRoughnessOperation;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Roughness - follows the definition in Wilson et al. (2007), which follows Dartnell (2000). <br>
+ * Roughness is the largest difference between any two cells
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterRoughnessProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(RasterRoughnessProcess.class);
+
+    public RasterRoughnessProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static GridCoverage2D process(GridCoverage2D inputCoverage, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(RasterRoughnessProcessFactory.inputCoverage.key, inputCoverage);
+
+        Process process = new RasterRoughnessProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (GridCoverage2D) resultMap.get(RasterRoughnessProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        GridCoverage2D inputCoverage = (GridCoverage2D) Params.getValue(input,
+                RasterRoughnessProcessFactory.inputCoverage, null);
+        if (inputCoverage == null) {
+            throw new NullPointerException("inputCoverage parameter required");
+        }
+
+        // start process
+        RasterRoughnessOperation process = new RasterRoughnessOperation();
+        GridCoverage2D extractedGC = process.execute(inputCoverage);
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(RasterRoughnessProcessFactory.RESULT.key, extractedGC);
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/gridcoverage/RasterRoughnessOperation.java
@@ -0,0 +1,103 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.gridcoverage;
+
+import java.util.logging.Logger;
+
+import javax.media.jai.iterator.RectIterFactory;
+import javax.media.jai.iterator.WritableRectIter;
+
+import org.geotools.coverage.grid.GridCoordinates2D;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.process.spatialstatistics.core.SSUtils;
+import org.geotools.process.spatialstatistics.enumeration.RasterPixelType;
+import org.geotools.util.logging.Logging;
+import org.jaitools.tiledimage.DiskMemImage;
+
+/**
+ * Roughness - follows the definition in Wilson et al. (2007), which follows Dartnell (2000). <br>
+ * Roughness is the largest difference between any two cells
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterRoughnessOperation extends AbstractSurfaceOperation {
+    protected static final Logger LOGGER = Logging.getLogger(RasterRoughnessOperation.class);
+
+    public GridCoverage2D execute(GridCoverage2D inputGc) {
+        DiskMemImage outputImage = this.createDiskMemImage(inputGc, RasterPixelType.FLOAT);
+
+        grid2D = inputGc;
+        srcNoData = RasterHelper.getNoDataValue(inputGc);
+        NoData = -9999;
+
+        final java.awt.Rectangle bounds = outputImage.getBounds();
+        WritableRectIter writer = RectIterFactory.createWritable(outputImage, bounds);
+
+        GridCoordinates2D pos = new GridCoordinates2D();
+
+        int y = 0;
+        writer.startLines();
+        while (!writer.finishedLines()) {
+
+            int x = 0;
+            writer.startPixels();
+            while (!writer.finishedPixels()) {
+                pos.setLocation(x, y);
+
+                visitRoughness(writer, pos);
+
+                writer.nextPixel();
+                x++;
+            }
+
+            writer.nextLine();
+            y++;
+        }
+
+        return createGridCoverage("Roughness", outputImage);
+    }
+
+    private void visitRoughness(WritableRectIter writer, GridCoordinates2D pos) {
+        // +-------+ +-------+
+        // | 0 1 2 | | a b c |
+        // | 3 4 5 |>| d e f |
+        // | 6 7 8 | | g h i |
+        // +-------+ +-------+
+        double[][] mx = getSubMatrix(grid2D, pos, 3, 3);
+        if (Double.isNaN(mx[1][1]) || SSUtils.compareDouble(srcNoData, mx[1][1])) {
+            writer.setSample(0, NoData);
+            return;
+        }
+
+        // Roughness is the largest difference between any two cells
+        double roughnessMin = mx[0][0];
+        double roughnessMax = mx[0][0];
+        for (int col = 0; col < 3; col++) {
+            for (int row = 0; row < 3; row++) {
+                roughnessMax = Math.max(roughnessMax, mx[col][row]);
+                roughnessMin = Math.min(roughnessMin, mx[col][row]);
+            }
+        }
+
+        double roughness = roughnessMax - roughnessMin;
+
+        writer.setSample(0, roughness);
+        updateStatistics(roughness);
+    }
+}

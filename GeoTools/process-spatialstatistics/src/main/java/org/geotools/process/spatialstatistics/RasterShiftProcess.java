c7fd0f2c5b83944514831309aa15328879c6988d
MapPlus <mapplus@gmail.com>
2018-06-29 22:04:33 +0900

    added raster shift process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterShiftProcess.java
@@ -0,0 +1,99 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterShiftOperation;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Moves the raster to a new geographic location, based on x and y shift values.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterShiftProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(RasterShiftProcess.class);
+
+    public RasterShiftProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static GridCoverage2D process(GridCoverage2D inputCoverage, Double xShift,
+            Double yShift, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(RasterShiftProcessFactory.inputCoverage.key, inputCoverage);
+        map.put(RasterShiftProcessFactory.xShift.key, xShift);
+        map.put(RasterShiftProcessFactory.yShift.key, yShift);
+
+        Process process = new RasterShiftProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (GridCoverage2D) resultMap.get(RasterShiftProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        GridCoverage2D inputCoverage = (GridCoverage2D) Params.getValue(input,
+                RasterShiftProcessFactory.inputCoverage, null);
+        if (inputCoverage == null) {
+            throw new NullPointerException("inputCoverage parameter required");
+        }
+
+        Double xShift = (Double) Params.getValue(input, RasterShiftProcessFactory.xShift,
+                RasterShiftProcessFactory.xShift.sample);
+
+        Double yShift = (Double) Params.getValue(input, RasterShiftProcessFactory.yShift,
+                RasterShiftProcessFactory.yShift.sample);
+
+        GridCoverage2D extractedGC = inputCoverage;
+
+        if (xShift != 0d || yShift != 0d) {
+            // start process
+            RasterShiftOperation process = new RasterShiftOperation();
+            extractedGC = process.execute(inputCoverage, xShift, yShift);
+            // end process
+        }
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(RasterShiftProcessFactory.RESULT.key, extractedGC);
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterShiftProcessFactory.java
@@ -0,0 +1,108 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.Parameter;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.InternationalString;
+
+/**
+ * RasterShiftProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterShiftProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging.getLogger(RasterShiftProcessFactory.class);
+
+    private static final String PROCESS_NAME = "RasterShift";
+
+    /*
+     * RasterShift(GridCoverage2D inputCoverage, Double xShift, Double yShift): GridCoverage2D
+     */
+
+    public RasterShiftProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new RasterShiftProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("RasterShift.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("RasterShift.description");
+    }
+
+    /** inputCoverage */
+    public static final Parameter<GridCoverage2D> inputCoverage = new Parameter<GridCoverage2D>(
+            "inputCoverage", GridCoverage2D.class, getResource("RasterShift.inputCoverage.title"),
+            getResource("RasterShift.inputCoverage.description"), true, 1, 1, null, null);
+
+    /** xShift */
+    public static final Parameter<Double> xShift = new Parameter<Double>("xShift", Double.class,
+            getResource("RasterShift.xShift.title"), getResource("RasterShift.xShift.description"),
+            false, 0, 1, Double.valueOf(0d), null);
+
+    /** yShift */
+    public static final Parameter<Double> yShift = new Parameter<Double>("yShift", Double.class,
+            getResource("RasterShift.yShift.title"), getResource("RasterShift.yShift.description"),
+            false, 0, 1, Double.valueOf(0d), null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputCoverage.key, inputCoverage);
+        parameterInfo.put(xShift.key, xShift);
+        parameterInfo.put(yShift.key, yShift);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<GridCoverage2D> RESULT = new Parameter<GridCoverage2D>("result",
+            GridCoverage2D.class, getResource("RasterShift.result.title"),
+            getResource("RasterShift.result.description"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}

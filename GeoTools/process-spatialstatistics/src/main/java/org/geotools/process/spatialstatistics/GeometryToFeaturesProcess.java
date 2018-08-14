0ec1e5ba8d2ae418337a9b32dd7dd49ee8e326fa
MapPlus <mapplus@gmail.com>
2018-08-14 10:09:20 +0900

    added geometry to features process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/GeometryToFeaturesProcess.java
@@ -0,0 +1,173 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.data.collection.ListFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.feature.simple.SimpleFeatureTypeBuilder;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.FeatureTypes;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.referencing.CRS;
+import org.geotools.util.logging.Logging;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.util.ProgressListener;
+
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.LineString;
+import com.vividsolutions.jts.geom.MultiLineString;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+
+/**
+ * Converts a geometry to features.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class GeometryToFeaturesProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(GeometryToFeaturesProcess.class);
+
+    public GeometryToFeaturesProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static Double process(Geometry geometry, CoordinateReferenceSystem crs, String typeName,
+            ProgressListener monitor) {
+        return GeometryToFeaturesProcess.process(geometry, crs, typeName, Boolean.FALSE, monitor);
+    }
+
+    public static Double process(Geometry geometry, CoordinateReferenceSystem crs, String typeName,
+            Boolean singlePart, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(GeometryToFeaturesProcessFactory.geometry.key, geometry);
+        map.put(GeometryToFeaturesProcessFactory.crs.key, crs);
+        map.put(GeometryToFeaturesProcessFactory.typeName.key, typeName);
+        map.put(GeometryToFeaturesProcessFactory.singlePart.key, singlePart);
+
+        Process process = new GeometryToFeaturesProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+            return (Double) resultMap.get(GeometryToFeaturesProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return new Double(0.0d);
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        Geometry geometry = (Geometry) Params.getValue(input,
+                GeometryToFeaturesProcessFactory.geometry, null);
+        CoordinateReferenceSystem crs = (CoordinateReferenceSystem) Params.getValue(input,
+                GeometryToFeaturesProcessFactory.crs, null);
+        String typeName = (String) Params.getValue(input,
+                GeometryToFeaturesProcessFactory.typeName,
+                GeometryToFeaturesProcessFactory.typeName.sample);
+        Boolean singlePart = (Boolean) Params.getValue(input,
+                GeometryToFeaturesProcessFactory.singlePart,
+                GeometryToFeaturesProcessFactory.singlePart.sample);
+        if (geometry == null) {
+            throw new NullPointerException("geometry parameter required");
+        }
+
+        // start process
+        // get the crs
+        if (crs == null && geometry.getUserData() instanceof CoordinateReferenceSystem) {
+            try {
+                crs = (CoordinateReferenceSystem) geometry.getUserData();
+            } catch (Exception e) {
+                // may not have a CRS attached
+            }
+        }
+
+        if (crs == null && geometry.getSRID() > 0) {
+            try {
+                crs = CRS.decode("EPSG:" + geometry.getSRID());
+            } catch (Exception e) {
+                // may not have a CRS attached
+            }
+        }
+
+        // build the feature type
+        Class<?> geometryBinding = geometry.getClass();
+        Class<?> targetGeometryBinding = geometry.getClass();
+
+        SimpleFeatureTypeBuilder typeBuilder = new SimpleFeatureTypeBuilder();
+        typeBuilder.setNamespaceURI(FeatureTypes.NAMESPACE_URL);
+        typeBuilder.setName(typeName);
+        typeBuilder.setCRS(crs);
+
+        typeBuilder.add("gid", Integer.class);
+
+        if (geometryBinding.isAssignableFrom(Polygon.class)) {
+            targetGeometryBinding = MultiPolygon.class;
+        } else if (geometryBinding.isAssignableFrom(LineString.class)) {
+            targetGeometryBinding = MultiLineString.class;
+        } else if (singlePart && geometryBinding.isAssignableFrom(MultiPoint.class)) {
+            targetGeometryBinding = Point.class;
+        }
+
+        // create shape field
+        typeBuilder.add(FeatureTypes.SHAPE_FIELD, targetGeometryBinding, crs);
+        SimpleFeatureType schema = typeBuilder.buildFeatureType();
+
+        ListFeatureCollection result = new ListFeatureCollection(schema);
+
+        // build the feature
+        SimpleFeatureBuilder builder = new SimpleFeatureBuilder(schema);
+        if (singlePart
+                && (MultiPolygon.class.equals(geometryBinding)
+                        || MultiLineString.class.equals(geometryBinding) || MultiPoint.class
+                            .equals(geometryBinding))) {
+
+            for (int index = 1; index <= geometry.getNumGeometries(); index++) {
+                Geometry part = geometry.getGeometryN(index - 1);
+
+                builder.addAll(new Object[] { index, part });
+                result.add(builder.buildFeature(typeName + "." + index));
+                builder.reset();
+            }
+        } else {
+            builder.addAll(new Object[] { 1, geometry });
+            result.add(builder.buildFeature(typeName + ".1"));
+        }
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(GeometryToFeaturesProcessFactory.RESULT.key, result);
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/GeometryToFeaturesProcessFactory.java
@@ -0,0 +1,120 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.data.Parameter;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.logging.Logging;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import org.opengis.util.InternationalString;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+/**
+ * GeometryToFeaturesProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class GeometryToFeaturesProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging
+            .getLogger(GeometryToFeaturesProcessFactory.class);
+
+    private static final String PROCESS_NAME = "GeometryToFeatures";
+
+    /*
+     * GeometryToFeatures(Geometry geometry, CoordinateReferenceSystem crs, String typeName, Boolean singlePart): SimpleFeatureCollection
+     */
+
+    public GeometryToFeaturesProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new GeometryToFeaturesProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("GeometryToFeatures.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("GeometryToFeatures.description");
+    }
+
+    /** inputFeatures */
+    public static final Parameter<Geometry> geometry = new Parameter<Geometry>("geometry",
+            Geometry.class, getResource("GeometryToFeatures.geometry.title"),
+            getResource("GeometryToFeatures.geometry.description"), true, 1, 1, null, null);
+
+    /** crs */
+    public static final Parameter<CoordinateReferenceSystem> crs = new Parameter<CoordinateReferenceSystem>(
+            "crs", CoordinateReferenceSystem.class, getResource("GeometryToFeatures.crs.title"),
+            getResource("GeometryToFeatures.crs.description"), false, 0, 1, null, null);
+
+    /** typeName */
+    public static final Parameter<String> typeName = new Parameter<String>("typeName",
+            String.class, getResource("GeometryToFeatures.typeName.title"),
+            getResource("GeometryToFeatures.typeName.description"), false, 0, 1, "Features", null);
+
+    /** singlePart */
+    public static final Parameter<Boolean> singlePart = new Parameter<Boolean>("singlePart",
+            Boolean.class, getResource("GeometryToFeatures.singlePart.title"),
+            getResource("GeometryToFeatures.singlePart.description"), false, 0, 1, Boolean.FALSE,
+            null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(geometry.key, geometry);
+        parameterInfo.put(crs.key, crs);
+        parameterInfo.put(typeName.key, typeName);
+        parameterInfo.put(singlePart.key, singlePart);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<SimpleFeatureCollection> RESULT = new Parameter<SimpleFeatureCollection>(
+            "result", SimpleFeatureCollection.class,
+            getResource("GeometryToFeatures.result.title"),
+            getResource("GeometryToFeatures.result.description"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}

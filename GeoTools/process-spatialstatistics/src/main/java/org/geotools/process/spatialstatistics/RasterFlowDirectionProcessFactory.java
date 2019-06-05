393ccb74439bf4a0b5646cbe6271a1986fd22667
MapPlus <mapplus@gmail.com>
2019-06-05 15:44:56 +0900

    added flow direction process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterFlowDirectionProcessFactory.java
@@ -0,0 +1,96 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2014, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.Parameter;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.logging.Logging;
+import org.opengis.util.InternationalString;
+
+/**
+ * RasterFlowDirectionProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterFlowDirectionProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging.getLogger(RasterFlowDirectionProcessFactory.class);
+
+    private static final String PROCESS_NAME = "RasterFlowDirection";
+
+    /*
+     * RasterFlowDirection(GridCoverage2D inputCoverage): GridCoverage2D
+     */
+
+    public RasterFlowDirectionProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new RasterFlowDirectionProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("RasterFlowDirection.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("RasterFlowDirection.description");
+    }
+
+    /** inputCoverage */
+    public static final Parameter<GridCoverage2D> inputCoverage = new Parameter<GridCoverage2D>(
+            "inputCoverage", GridCoverage2D.class, getResource("RasterFlowDirection.inputCoverage.title"),
+            getResource("RasterFlowDirection.inputCoverage.description"), true, 1, 1, null, null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputCoverage.key, inputCoverage);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<GridCoverage2D> RESULT = new Parameter<GridCoverage2D>("result",
+            GridCoverage2D.class, getResource("RasterFlowDirection.result.title"),
+            getResource("RasterFlowDirection.result.description"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}

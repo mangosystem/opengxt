7acf650e0c3f028154af3a28db88f7052eecdaab
jyajya <jya1210@gmail.com>
2020-05-11 19:26:38 +0900

    add multi wind rose map

--- GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/MultiWindRoseMapProcess.java
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/MultiWindRoseMapProcess.java
@@ -44,136 +44,142 @@ import com.vividsolutions.jts.geom.Geometry;
  * @source $URL$
  */
 public class MultiWindRoseMapProcess extends AbstractStatisticsProcess {
-        protected static final Logger LOGGER = Logging.getLogger(MultiWindRoseMapProcess.class);
+    protected static final Logger LOGGER = Logging.getLogger(MultiWindRoseMapProcess.class);
 
-        private boolean started = false;
+    private boolean started = false;
 
-        public MultiWindRoseMapProcess(ProcessFactory factory) {
-                super(factory);
+    public MultiWindRoseMapProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static SimpleFeatureCollection process(Collection<SimpleFeatureCollection> inputFeatures,
+            String weightFields, SimpleFeatureCollection centerFeatures, Geometry centerPoint,
+            Double searchRadius, String valueField, int roseCnt, ProgressListener monitor) {
+        // SimpleFeatureCollection inputFeatures, String weightField,
+        // SimpleFeatureCollection centerFeatures, double searchRadius, String valueField, int roseCnt
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(MultiWindRoseMapProcessFactory.inputFeatures.key, inputFeatures);
+        map.put(MultiWindRoseMapProcessFactory.weightFields.key, weightFields);
+        // map.put(VA_WindRoseFactory.inputFeatures2.key, inputFeatures[1]);
+        // map.put(VA_WindRoseFactory.weightField2.key, weightField[1]);
+        // map.put(VA_WindRoseFactory.inputFeatures3.key, inputFeatures[2]);
+        // map.put(VA_WindRoseFactory.weightField3.key, weightField[2]);
+        map.put(MultiWindRoseMapProcessFactory.centerFeatures.key, centerFeatures);
+        map.put(MultiWindRoseMapProcessFactory.centerPoint.key, centerPoint);
+        map.put(MultiWindRoseMapProcessFactory.searchRadius.key, searchRadius);
+        map.put(MultiWindRoseMapProcessFactory.roseCount.key, roseCnt);
+
+        // parameterInfo.put(inputFeatures.key, inputFeatures);
+        // parameterInfo.put(weightField.key, weightField);
+        // parameterInfo.put(valueField.key, valueField);
+        // parameterInfo.put(roseCount.key, roseCount);
+
+        Process process = new MultiWindRoseMapProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (SimpleFeatureCollection) resultMap
+                    .get(MultiWindRoseMapProcessFactory.result.key);
+        } catch (ProcessException e) {
+            e.printStackTrace();
+            LOGGER.log(Level.FINER, e.getMessage(), e);
         }
 
-        public ProcessFactory getFactory() {
-                return factory;
-        }
+        return null;
+    }
 
-        public static SimpleFeatureCollection process(Collection<SimpleFeatureCollection> inputFeatures, String weightFields,
-                        SimpleFeatureCollection centerFeatures, Geometry centerPoint, Double searchRadius, String valueField, int roseCnt,
-                        ProgressListener monitor) {
-//        SimpleFeatureCollection inputFeatures, String weightField,
-//        SimpleFeatureCollection centerFeatures, double searchRadius, String valueField, int roseCnt
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put(MultiWindRoseMapProcessFactory.inputFeatures.key, inputFeatures);
-                map.put(MultiWindRoseMapProcessFactory.weightFields.key, weightFields);
-//        map.put(VA_WindRoseFactory.inputFeatures2.key, inputFeatures[1]);
-//        map.put(VA_WindRoseFactory.weightField2.key, weightField[1]);
-//        map.put(VA_WindRoseFactory.inputFeatures3.key, inputFeatures[2]);
-//        map.put(VA_WindRoseFactory.weightField3.key, weightField[2]);
-                map.put(MultiWindRoseMapProcessFactory.centerFeatures.key, centerFeatures);
-                map.put(MultiWindRoseMapProcessFactory.centerPoint.key, centerPoint);
-                map.put(MultiWindRoseMapProcessFactory.searchRadius.key, searchRadius);
-                map.put(MultiWindRoseMapProcessFactory.roseCount.key, roseCnt);
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        if (started)
+            throw new IllegalStateException("Process can only be run once");
+        started = true;
 
-                // parameterInfo.put(inputFeatures.key, inputFeatures);
-                // parameterInfo.put(weightField.key, weightField);
-                // parameterInfo.put(valueField.key, valueField);
-                // parameterInfo.put(roseCount.key, roseCount);
+        if (monitor == null)
+            monitor = new NullProgressListener();
+        try {
+            monitor.started();
+            monitor.setTask(Text.text("Grabbing arguments"));
+            monitor.progress(10.0f);
 
-                Process process = new MultiWindRoseMapProcess(null);
-                Map<String, Object> resultMap;
-                try {
-                        resultMap = process.execute(map, monitor);
+            monitor.setTask(Text.text("Processing " + this.getClass().getSimpleName()));
+            monitor.progress(25.0f);
 
-                        return (SimpleFeatureCollection) resultMap.get(MultiWindRoseMapProcessFactory.result.key);
-                } catch (ProcessException e) {
-                        e.printStackTrace();
-                        LOGGER.log(Level.FINER, e.getMessage(), e);
+            if (monitor.isCanceled()) {
+                return null; // user has canceled this operation
+            }
+
+            Collection<SimpleFeatureCollection> inputFeatures = (Collection<SimpleFeatureCollection>) Params
+                    .getValue(input, MultiWindRoseMapProcessFactory.inputFeatures, null);
+
+            String weightFiels = (String) Params.getValue(input,
+                    MultiWindRoseMapProcessFactory.weightFields, null);
+
+            // SimpleFeatureCollection inputFeatures2 = (SimpleFeatureCollection) ParamUtil.getParam(
+            // input, MultiWindRoseMapProcessFactory.inputFeatures2, null);
+            //
+            // String weightField2 = (String) ParamUtil.getParam(input, VA_WindRoseFactory.weightField2,
+            // null);
+            // SimpleFeatureCollection inputFeatures3 = (SimpleFeatureCollection) ParamUtil.getParam(
+            // input, MultiWindRoseMapProcessFactory.inputFeatures3, null);
+            //
+            // String weightField3 = (String) ParamUtil.getParam(input, VA_WindRoseFactory.weightField3,
+            // null);
+            SimpleFeatureCollection centerFeatures = (SimpleFeatureCollection) Params
+                    .getValue(input, MultiWindRoseMapProcessFactory.centerFeatures, null);
+            Geometry centerPoint = (Geometry) Params.getValue(input,
+                    MultiWindRoseMapProcessFactory.centerPoint, null);
+            Double searchRadius = (Double) Params.getValue(input,
+                    MultiWindRoseMapProcessFactory.searchRadius, null);
+            Integer roseCount = (Integer) Params.getValue(input,
+                    MultiWindRoseMapProcessFactory.roseCount, Integer.valueOf(36));
+
+            // start process
+            SimpleFeatureCollection resultFc = null;
+            SimpleFeatureCollection anchorFc = null;
+            try {
+                SimpleFeatureSource sfc = null;
+                Collection<SimpleFeatureCollection> tgInputFeatures = inputFeatures;
+                String[] tgWeightField = weightFiels.split(",");
+                MultiWindRoseOperation process = new MultiWindRoseOperation();
+                if (centerFeatures.size() > 0) {
+                    sfc = process.execute(tgInputFeatures, tgWeightField, centerFeatures,
+                            searchRadius, roseCount);
+                } else {
+                    sfc = process.execute(tgInputFeatures, tgWeightField, centerPoint, searchRadius,
+                            roseCount);
                 }
 
-                return null;
+                resultFc = sfc == null ? null : sfc.getFeatures();
+                anchorFc = process.getAnchor().getFeatures();
+            } catch (Exception ee) {
+                ee.printStackTrace();
+                monitor.exceptionOccurred(ee);
+            }
+            // end process
+
+            monitor.setTask(Text.text("Encoding result"));
+            monitor.progress(90.0f);
+
+            Map<String, Object> resultMap = new HashMap<String, Object>();
+            resultMap.put(MultiWindRoseMapProcessFactory.result.key, resultFc);
+            resultMap.put(MultiWindRoseMapProcessFactory.anchor.key, anchorFc);
+            // resultMap.put(VA_RingMapFactory.ring_anchor.key, anchorFc);
+            monitor.complete(); // same as 100.0f
+
+            return resultMap;
+        } catch (Exception eek) {
+            eek.printStackTrace();
+            monitor.exceptionOccurred(eek);
+            return null;
+        } finally {
+            monitor.dispose();
         }
-
-        @Override
-        public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor) throws ProcessException {
-                if (started)
-                        throw new IllegalStateException("Process can only be run once");
-                started = true;
-
-                if (monitor == null)
-                        monitor = new NullProgressListener();
-                try {
-                        monitor.started();
-                        monitor.setTask(Text.text("Grabbing arguments"));
-                        monitor.progress(10.0f);
-
-                        monitor.setTask(Text.text("Processing " + this.getClass().getSimpleName()));
-                        monitor.progress(25.0f);
-
-                        if (monitor.isCanceled()) {
-                                return null; // user has canceled this operation
-                        }
-
-                Collection<SimpleFeatureCollection> inputFeatures = (Collection<SimpleFeatureCollection>) Params.getValue(input,
-                                        MultiWindRoseMapProcessFactory.inputFeatures, null);
-
-                        String weightFiels = (String) Params.getValue(input, MultiWindRoseMapProcessFactory.weightFields, null);
-
-//            SimpleFeatureCollection inputFeatures2 = (SimpleFeatureCollection) ParamUtil.getParam(
-//                    input, MultiWindRoseMapProcessFactory.inputFeatures2, null);
-//
-//            String weightField2 = (String) ParamUtil.getParam(input, VA_WindRoseFactory.weightField2,
-//                    null);
-//            SimpleFeatureCollection inputFeatures3 = (SimpleFeatureCollection) ParamUtil.getParam(
-//                    input, MultiWindRoseMapProcessFactory.inputFeatures3, null);
-//
-//            String weightField3 = (String) ParamUtil.getParam(input, VA_WindRoseFactory.weightField3,
-//                    null);
-                        SimpleFeatureCollection centerFeatures = (SimpleFeatureCollection) Params.getValue(input,
-                                        MultiWindRoseMapProcessFactory.centerFeatures, null);
-                        Geometry centerPoint = (Geometry) Params.getValue(input,
-                                        MultiWindRoseMapProcessFactory.centerPoint, null);
-                        Double searchRadius = (Double) Params.getValue(input, MultiWindRoseMapProcessFactory.searchRadius, null);
-                        Integer roseCount = (Integer) Params.getValue(input, MultiWindRoseMapProcessFactory.roseCount,
-                                        Integer.valueOf(36));
-
-                        // start process
-                        SimpleFeatureCollection resultFc = null;
-                        SimpleFeatureCollection anchorFc = null;
-                        try {
-                                SimpleFeatureSource sfc = null;
-                                Collection<SimpleFeatureCollection> tgInputFeatures = inputFeatures;
-                                String[] tgWeightField = weightFiels.split(",");
-                                MultiWindRoseOperation process = new MultiWindRoseOperation();
-                                if(centerFeatures.size() > 0) {
-                                        sfc = process.execute(tgInputFeatures, tgWeightField, centerFeatures, searchRadius, roseCount);
-                                } else {
-                                        sfc = process.execute(tgInputFeatures, tgWeightField, centerPoint, searchRadius, roseCount);
-                                }
-
-                                resultFc = sfc == null ? null : sfc.getFeatures();
-                                anchorFc = process.getAnchor().getFeatures();
-                        } catch (Exception ee) {
-                                ee.printStackTrace();
-                                monitor.exceptionOccurred(ee);
-                        }
-                        // end process
-
-                        monitor.setTask(Text.text("Encoding result"));
-                        monitor.progress(90.0f);
-
-                        Map<String, Object> resultMap = new HashMap<String, Object>();
-                        resultMap.put(MultiWindRoseMapProcessFactory.result.key, resultFc);
-                        resultMap.put(MultiWindRoseMapProcessFactory.anchor.key, anchorFc);
-                        // resultMap.put(VA_RingMapFactory.ring_anchor.key, anchorFc);
-                        monitor.complete(); // same as 100.0f
-
-                        return resultMap;
-                } catch (Exception eek) {
-                        eek.printStackTrace();
-                        monitor.exceptionOccurred(eek);
-                        return null;
-                } finally {
-                        monitor.dispose();
-                }
-        }
+    }
 
 }
--- GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/MultiWindRoseMapProcessFactory.java
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/MultiWindRoseMapProcessFactory.java
@@ -52,71 +52,70 @@ public class MultiWindRoseMapProcessFactory extends SpatialStatisticsProcessFact
     public MultiWindRoseMapProcessFactory() {
         super(new NameImpl(NAMESPACE, PROCESS_NAME));
     }
-//  SimpleFeatureCollection inputFeatures, String weightField,
-//  SimpleFeatureCollection centerFeatures, double searchRadius, String valueField, int roseCnt
+    // SimpleFeatureCollection inputFeatures, String weightField,
+    // SimpleFeatureCollection centerFeatures, double searchRadius, String valueField, int roseCnt
 
     /** inputFeatures */
-	public static final Parameter<SimpleFeatureCollection> inputFeatures = new Parameter<SimpleFeatureCollection>(
-			"inputFeatures", SimpleFeatureCollection.class, 
-			getResource("MultiWindRoseMap.inputFeatures.title"),
-			getResource("MultiWindRoseMap.inputFeatures.description"), true, 1, Integer.MAX_VALUE, null, null);
+    public static final Parameter<SimpleFeatureCollection> inputFeatures = new Parameter<SimpleFeatureCollection>(
+            "inputFeatures", SimpleFeatureCollection.class,
+            getResource("MultiWindRoseMap.inputFeatures.title"),
+            getResource("MultiWindRoseMap.inputFeatures.description"), true, 1, Integer.MAX_VALUE,
+            null, null);
 
-	/** weightField */
-	public static final Parameter<String[]> weightFields = new Parameter<String[]>(
-			"weightField", String[].class,
-			getResource("MultiWindRoseMap.weightFields.title"),
-			getResource("MultiWindRoseMap.weightFields.description"), false, 0, 1, null, null);
-    
-//    /** inputFeatures */
-//    public static final Parameter<SimpleFeatureCollection> inputFeatures2 = new Parameter<SimpleFeatureCollection>(
-//            "inputFeatures2", SimpleFeatureCollection.class, Text.text("Input Point Layer 2"),
-//            Text.text("Input Vector Layer"), false, 1, 1, null, null);
-//
-//    /** weightField */
-//    public static final Parameter<String> weightField2 = new Parameter<String>(
-//            "weightField2", String.class, Text.text("Field to apply the weight(Point Layer 2)"),
-//            Text.text("Field to apply the weight"), false, 0, 1, null, null);
-//    
-//    /** inputFeatures */
-//    public static final Parameter<SimpleFeatureCollection> inputFeatures3 = new Parameter<SimpleFeatureCollection>(
-//            "inputFeatures3", SimpleFeatureCollection.class, Text.text("Input Point Layer 3"),
-//            Text.text("Input Vector Layer"), false, 1, 1, null, null);
-//
-//    /** weightField */
-//    public static final Parameter<String> weightField3 = new Parameter<String>(
-//            "weightField3", String.class, Text.text("Field to apply the weight(Point Layer 3)"),
-//            Text.text("Field to apply the weight"), false, 0, 1, null, null);
-    
+    /** weightField */
+    public static final Parameter<String[]> weightFields = new Parameter<String[]>("weightField",
+            String[].class, getResource("MultiWindRoseMap.weightFields.title"),
+            getResource("MultiWindRoseMap.weightFields.description"), false, 0, Integer.MAX_VALUE, null, null);
+
+    // /** inputFeatures */
+    // public static final Parameter<SimpleFeatureCollection> inputFeatures2 = new Parameter<SimpleFeatureCollection>(
+    // "inputFeatures2", SimpleFeatureCollection.class, Text.text("Input Point Layer 2"),
+    // Text.text("Input Vector Layer"), false, 1, 1, null, null);
+    //
+    // /** weightField */
+    // public static final Parameter<String> weightField2 = new Parameter<String>(
+    // "weightField2", String.class, Text.text("Field to apply the weight(Point Layer 2)"),
+    // Text.text("Field to apply the weight"), false, 0, 1, null, null);
+    //
+    // /** inputFeatures */
+    // public static final Parameter<SimpleFeatureCollection> inputFeatures3 = new Parameter<SimpleFeatureCollection>(
+    // "inputFeatures3", SimpleFeatureCollection.class, Text.text("Input Point Layer 3"),
+    // Text.text("Input Vector Layer"), false, 1, 1, null, null);
+    //
+    // /** weightField */
+    // public static final Parameter<String> weightField3 = new Parameter<String>(
+    // "weightField3", String.class, Text.text("Field to apply the weight(Point Layer 3)"),
+    // Text.text("Field to apply the weight"), false, 0, 1, null, null);
+
     /** centerFeatures */
     public static final Parameter<SimpleFeatureCollection> centerFeatures = new Parameter<SimpleFeatureCollection>(
-            "centerFeatures", SimpleFeatureCollection.class, 
+            "centerFeatures", SimpleFeatureCollection.class,
             getResource("MultiWindRoseMap.centerFeatures.title"),
             getResource("MultiWindRoseMap.centerFeatures.description"), false, 0, 1, null, null);
-    public static final Parameter<Geometry> centerPoint = new Parameter<Geometry>(
-            "centerPoint", Geometry.class, 
-            getResource("MultiWindRoseMap.centerPoint.title"),
+
+    public static final Parameter<Geometry> centerPoint = new Parameter<Geometry>("centerPoint",
+            Geometry.class, getResource("MultiWindRoseMap.centerPoint.title"),
             getResource("MultiWindRoseMap.centerPoint.description"), false, 0, 1, null, null);
 
     /** searchRadius */
-    public static final Parameter<Double> searchRadius = new Parameter<Double>(
-            "searchRadius", Double.class, 
-            getResource("MultiWindRoseMap.searchRadius.title"),
+    public static final Parameter<Double> searchRadius = new Parameter<Double>("searchRadius",
+            Double.class, getResource("MultiWindRoseMap.searchRadius.title"),
             getResource("MultiWindRoseMap.searchRadius.description"), false, 0, 1, null, null);
-    
+
     /** roseCount */
-    public static final Parameter<Integer> roseCount = new Parameter<Integer>("roseCount", Integer.class,
-    		getResource("MultiWindRoseMap.roseCount.title"), 
-    		getResource("MultiWindRoseMap.roseCount.description"), true, 0, 1, 16, null);
+    public static final Parameter<Integer> roseCount = new Parameter<Integer>("roseCount",
+            Integer.class, getResource("MultiWindRoseMap.roseCount.title"),
+            getResource("MultiWindRoseMap.roseCount.description"), true, 0, 1, 16, null);
 
     @Override
     protected Map<String, Parameter<?>> getParameterInfo() {
         HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
         parameterInfo.put(inputFeatures.key, inputFeatures);
         parameterInfo.put(weightFields.key, weightFields);
-//        parameterInfo.put(inputFeatures2.key, inputFeatures2);
-//        parameterInfo.put(weightField2.key, weightField2);
-//        parameterInfo.put(inputFeatures3.key, inputFeatures3);
-//        parameterInfo.put(weightField3.key, weightField3);
+        // parameterInfo.put(inputFeatures2.key, inputFeatures2);
+        // parameterInfo.put(weightField2.key, weightField2);
+        // parameterInfo.put(inputFeatures3.key, inputFeatures3);
+        // parameterInfo.put(weightField3.key, weightField3);
         parameterInfo.put(centerFeatures.key, centerFeatures);
         parameterInfo.put(centerPoint.key, centerPoint);
         parameterInfo.put(searchRadius.key, searchRadius);
@@ -125,11 +124,12 @@ public class MultiWindRoseMapProcessFactory extends SpatialStatisticsProcessFact
     }
 
     public static final Parameter<SimpleFeatureCollection> result = new Parameter<SimpleFeatureCollection>(
-            "multi_wind_rose", SimpleFeatureCollection.class, 
+            "multi_wind_rose", SimpleFeatureCollection.class,
             getResource("MultiWindRoseMap.result.title"),
             getResource("MultiWindRoseMap.result.description"));
+
     public static final Parameter<SimpleFeatureCollection> anchor = new Parameter<SimpleFeatureCollection>(
-            "multi_wind_rose_anchor", SimpleFeatureCollection.class, 
+            "multi_wind_rose_anchor", SimpleFeatureCollection.class,
             getResource("MultiWindRoseMap.anchor.title"),
             getResource("MultiWindRoseMap.anchor.description"));
 
@@ -149,7 +149,7 @@ public class MultiWindRoseMapProcessFactory extends SpatialStatisticsProcessFact
     public Process create() {
         return new MultiWindRoseMapProcess(this);
     }
-    
+
     @Override
     public InternationalString getTitle() {
         return getResource("MultiWindRoseMap.title");

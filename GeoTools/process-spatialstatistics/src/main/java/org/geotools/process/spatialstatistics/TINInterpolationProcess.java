c5e7079a4d2cbe6a4ec8572f6615750070151a79
mapplus <mapplus@gmail.com>
2020-09-04 08:58:39 +0900

    Add TIN Interpolation process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/TINInterpolationProcess.java
@@ -0,0 +1,124 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.enumeration.RasterPixelType;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterInterpolationTINOperation;
+import org.geotools.util.logging.Logging;
+import org.opengis.filter.expression.Expression;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Interpolates a raster surface from points using an Triangulated Irregular Network(TIN) technique.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class TINInterpolationProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(TINInterpolationProcess.class);
+
+    public TINInterpolationProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static GridCoverage2D process(SimpleFeatureCollection inputFeatures,
+            Expression inputField, RasterPixelType pixelType, Double cellSize,
+            ReferencedEnvelope extent, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(TINInterpolationProcessFactory.inputFeatures.key, inputFeatures);
+        map.put(TINInterpolationProcessFactory.inputField.key, inputField);
+        map.put(TINInterpolationProcessFactory.pixelType.key, pixelType);
+
+        map.put(TINInterpolationProcessFactory.cellSize.key, cellSize);
+        map.put(TINInterpolationProcessFactory.extent.key, extent);
+
+        Process process = new TINInterpolationProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (GridCoverage2D) resultMap.get(TINInterpolationProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        SimpleFeatureCollection inputFeatures = (SimpleFeatureCollection) Params.getValue(input,
+                TINInterpolationProcessFactory.inputFeatures, null);
+        Expression inputField = (Expression) Params.getValue(input,
+                TINInterpolationProcessFactory.inputField, null);
+        if (inputFeatures == null || inputField == null) {
+            throw new NullPointerException("inputFeatures, inputField parameters required");
+        }
+
+        RasterPixelType pixelType = (RasterPixelType) Params.getValue(input,
+                TINInterpolationProcessFactory.pixelType,
+                TINInterpolationProcessFactory.pixelType.sample);
+
+        Double cellSize = (Double) Params.getValue(input, TINInterpolationProcessFactory.cellSize,
+                TINInterpolationProcessFactory.cellSize.sample);
+        ReferencedEnvelope extent = (ReferencedEnvelope) Params.getValue(input,
+                TINInterpolationProcessFactory.extent, null);
+
+        // start process
+        ReferencedEnvelope boundingBox = extent == null ? inputFeatures.getBounds() : extent;
+
+        // get default cell size from extent
+        if (cellSize == 0.0) {
+            cellSize = Math.min(boundingBox.getWidth(), boundingBox.getHeight()) / 250.0;
+            LOGGER.warning("default cell size = " + cellSize);
+        }
+
+        GridCoverage2D resultGc = null;
+        RasterInterpolationTINOperation process = new RasterInterpolationTINOperation();
+        process.getRasterEnvironment().setExtent(boundingBox);
+
+        if (cellSize > 0) {
+            process.getRasterEnvironment().setCellSizeX(cellSize);
+            process.getRasterEnvironment().setCellSizeY(cellSize);
+        }
+
+        resultGc = process.execute(inputFeatures, inputField, pixelType);
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(TINInterpolationProcessFactory.RESULT.key, resultGc);
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/gridcoverage/RasterInterpolationTINOperation.java
@@ -0,0 +1,195 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.gridcoverage;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.media.jai.iterator.RectIterFactory;
+import javax.media.jai.iterator.WritableRectIter;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.geometry.jts.JTSFactoryFinder;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.process.spatialstatistics.enumeration.RasterPixelType;
+import org.geotools.util.factory.GeoTools;
+import org.geotools.util.logging.Logging;
+import org.jaitools.tiledimage.DiskMemImage;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.CoordinateList;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.MultiPolygon;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.geom.Triangle;
+import org.locationtech.jts.geom.prep.PreparedGeometry;
+import org.locationtech.jts.geom.prep.PreparedGeometryFactory;
+import org.locationtech.jts.triangulate.DelaunayTriangulationBuilder;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.expression.Expression;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+/**
+ * Interpolates a raster surface from points using an Triangulated Irregular Network(TIN) technique.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterInterpolationTINOperation extends RasterProcessingOperation {
+    protected static final Logger LOGGER = Logging.getLogger(RasterInterpolationTINOperation.class);
+
+    private GeometryFactory gf = JTSFactoryFinder.getGeometryFactory(GeoTools.getDefaultHints());
+
+    private FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(null);
+
+    private double proximalTolerance = 0.0d;
+
+    public RasterInterpolationTINOperation() {
+
+    }
+
+    public double getProximalTolerance() {
+        return proximalTolerance;
+    }
+
+    public void setProximalTolerance(double proximalTolerance) {
+        this.proximalTolerance = proximalTolerance;
+    }
+
+    public GridCoverage2D execute(SimpleFeatureCollection features, String inputField) {
+        return execute(features, inputField, RasterPixelType.FLOAT);
+    }
+
+    public GridCoverage2D execute(SimpleFeatureCollection features, String inputField,
+            RasterPixelType pixelType) {
+        return execute(features, ff.property(inputField), pixelType);
+    }
+
+    public GridCoverage2D execute(SimpleFeatureCollection features, Expression inputField) {
+        return execute(features, inputField, RasterPixelType.FLOAT);
+    }
+
+    public GridCoverage2D execute(SimpleFeatureCollection features, Expression inputField,
+            RasterPixelType pixelType) {
+        // create delaunay triangulation
+        List<Coordinate> coordinates = getCoordinateList(features, inputField);
+
+        // Gets the faces of the computed triangulation as a GeometryCollection of Polygon.
+        DelaunayTriangulationBuilder vdBuilder = new DelaunayTriangulationBuilder();
+        vdBuilder.setSites(coordinates);
+        vdBuilder.setTolerance(proximalTolerance);
+        Geometry triangles = vdBuilder.getTriangles(gf);
+        coordinates.clear();
+
+        // calculate extent & cellsize
+        calculateExtentAndCellSize(features, RasterHelper.getDefaultNoDataValue(pixelType));
+
+        // create image & write pixels
+        final DiskMemImage oi = createDiskMemImage(Extent, pixelType);
+        initializeDefaultValue(oi, this.NoData);
+
+        final GridTransformer trans = new GridTransformer(Extent, CellSizeX, CellSizeY);
+
+        CoordinateReferenceSystem crs = features.getSchema().getCoordinateReferenceSystem();
+        ReferencedEnvelope envelope = null;
+
+        for (int index = 0; index < triangles.getNumGeometries(); index++) {
+            Geometry triangle = triangles.getGeometryN(index);
+            if (triangle == null || triangle.isEmpty()) {
+                continue;
+            }
+
+            PreparedGeometry preparedGeom = PreparedGeometryFactory.prepare(triangle);
+
+            Coordinate[] coords = triangle.getCoordinates();
+
+            Coordinate v0 = coords[0];
+            Coordinate v1 = coords[1];
+            Coordinate v2 = coords[2];
+
+            // write raster
+            envelope = new ReferencedEnvelope(triangle.getEnvelopeInternal(), crs);
+            java.awt.Rectangle rect = trans.worldToGridBounds(envelope);
+
+            WritableRectIter writer = RectIterFactory.createWritable(oi, rect);
+
+            writer.startLines();
+            int y = rect.y;
+            while (!writer.finishedLines()) {
+                writer.startPixels();
+                int x = rect.x;
+                while (!writer.finishedPixels()) {
+                    Coordinate p = trans.gridToWorldCoordinate(x, y);
+
+                    if (!preparedGeom.disjoint(gf.createPoint(p))) {
+                        double interpolated = Triangle.interpolateZ(p, v0, v1, v2);
+                        if (Double.isNaN(interpolated) || Double.isInfinite(interpolated)) {
+                            interpolated = this.NoData;
+                        }
+
+                        writer.setSample(0, interpolated);
+                        updateStatistics(interpolated);
+                    }
+
+                    writer.nextPixel();
+                    x++;
+                }
+                writer.nextLine();
+                y++;
+            }
+        }
+
+        return createGridCoverage("TIN", oi);
+    }
+
+    private List<Coordinate> getCoordinateList(SimpleFeatureCollection features,
+            Expression inputField) {
+        CoordinateList coordinateList = new CoordinateList();
+        SimpleFeatureIterator featureIter = features.features();
+        try {
+            while (featureIter.hasNext()) {
+                SimpleFeature feature = featureIter.next();
+
+                Double zValue = inputField.evaluate(feature, Double.class);
+                if (zValue == null || zValue.isNaN() || zValue.isInfinite()) {
+                    continue;
+                }
+
+                Geometry geometry = (Geometry) feature.getDefaultGeometry();
+                Coordinate[] coords = geometry.getCoordinates();
+                int size = geometry instanceof MultiPolygon || geometry instanceof Polygon
+                        ? coords.length - 1
+                        : coords.length;
+
+                for (int i = 0; i < size; i++) {
+                    Coordinate xyzCoordnate = new Coordinate(coords[i]);
+                    xyzCoordnate.setZ(zValue);
+                    coordinateList.add(xyzCoordnate, false);
+                }
+            }
+        } finally {
+            featureIter.close();
+        }
+
+        return coordinateList;
+    }
+}

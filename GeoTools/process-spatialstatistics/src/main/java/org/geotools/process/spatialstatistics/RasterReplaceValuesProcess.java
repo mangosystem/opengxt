ce534540fb798f219d76517653a6952538848d06
mapplus <mapplus@gmail.com>
2020-11-08 14:23:48 +0900

    Add raster replace values process

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterReplaceValuesProcess.java
@@ -0,0 +1,95 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterReplaceValuesOperation;
+import org.geotools.util.logging.Logging;
+import org.locationtech.jts.geom.Geometry;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Replace raster values within polygon with specific value.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterReplaceValuesProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(RasterReplaceValuesProcess.class);
+
+    public RasterReplaceValuesProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static GridCoverage2D process(GridCoverage2D inputCoverage, Geometry region,
+            Double replaceValue, ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(RasterReplaceValuesProcessFactory.inputCoverage.key, inputCoverage);
+        map.put(RasterReplaceValuesProcessFactory.region.key, region);
+        map.put(RasterReplaceValuesProcessFactory.replaceValue.key, replaceValue);
+
+        Process process = new RasterReplaceValuesProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (GridCoverage2D) resultMap.get(RasterReplaceValuesProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        GridCoverage2D inputCoverage = (GridCoverage2D) Params.getValue(input,
+                RasterReplaceValuesProcessFactory.inputCoverage, null);
+        Geometry region = (Geometry) Params.getValue(input,
+                RasterReplaceValuesProcessFactory.region, null);
+        Double replaceValue = (Double) Params.getValue(input,
+                RasterReplaceValuesProcessFactory.replaceValue, null);
+        if (inputCoverage == null || region == null || replaceValue == null) {
+            throw new NullPointerException(
+                    "inputCoverage, region, replaceValue parameters required");
+        }
+
+        // start process
+        RasterReplaceValuesOperation process = new RasterReplaceValuesOperation();
+        GridCoverage2D resultGc = process.execute(inputCoverage, region, replaceValue);
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(RasterReplaceValuesProcessFactory.RESULT.key, resultGc);
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/gridcoverage/RasterReplaceValuesOperation.java
@@ -0,0 +1,130 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics.gridcoverage;
+
+import java.awt.image.WritableRaster;
+import java.util.logging.Logger;
+
+import javax.media.jai.PlanarImage;
+import javax.media.jai.iterator.RectIter;
+import javax.media.jai.iterator.RectIterFactory;
+
+import org.geotools.coverage.grid.GridCoordinates2D;
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.process.spatialstatistics.core.SSUtils;
+import org.geotools.process.spatialstatistics.enumeration.RasterPixelType;
+import org.geotools.util.logging.Logging;
+import org.jaitools.tiledimage.DiskMemImage;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+/**
+ * Replace raster values within polygon with specific value.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterReplaceValuesOperation extends RasterProcessingOperation {
+    protected static final Logger LOGGER = Logging.getLogger(RasterReplaceValuesOperation.class);
+
+    public RasterReplaceValuesOperation() {
+
+    }
+
+    public GridCoverage2D execute(GridCoverage2D inputCoverage, Geometry region,
+            double replaceValue) {
+        if (region == null || region.isEmpty()) {
+            return inputCoverage;
+        }
+
+        // create template raster
+        RasterPixelType pixelType = RasterHelper.getTransferType(inputCoverage);
+        DiskMemImage outputImage = this.createDiskMemImage(inputCoverage, pixelType);
+        outputImage.setData(inputCoverage.getRenderedImage().getData());
+
+        WritableRaster outputRaster = (WritableRaster) outputImage.getData();
+
+        this.NoData = RasterHelper.getNoDataValue(inputCoverage);
+        GridTransformer inputTrans = new GridTransformer(inputCoverage);
+
+        // convert region geometry to raster
+        CoordinateReferenceSystem crs = inputCoverage.getCoordinateReferenceSystem();
+        Geometry aoiGeom = super.transformGeometry(region, crs);
+
+        RasterEnvironment rasterEnv = new RasterEnvironment();
+        rasterEnv.setCellSizeX(CellSizeX);
+        rasterEnv.setCellSizeY(CellSizeY);
+        rasterEnv.setExtent(new ReferencedEnvelope(aoiGeom.getEnvelopeInternal(), crs));
+
+        GeometryToRasterOperation geometryToRaster = new GeometryToRasterOperation();
+        geometryToRaster.setRasterEnvironment(rasterEnv);
+
+        GridCoverage2D aoiGc = geometryToRaster.execute(aoiGeom, crs, replaceValue, pixelType);
+        double aoiNoData = RasterHelper.getNoDataValue(aoiGc);
+        GridTransformer aoiTrans = new GridTransformer(aoiGc);
+
+        // replace values
+        PlanarImage inputImage = (PlanarImage) aoiGc.getRenderedImage();
+        RectIter readIter = RectIterFactory.create(inputImage, inputImage.getBounds());
+
+        int valid = 0;
+        int row = 0;
+        readIter.startLines();
+        while (!readIter.finishedLines()) {
+            int column = 0;
+            readIter.startPixels();
+
+            while (!readIter.finishedPixels()) {
+                final double gridVal = readIter.getSampleDouble(0);
+
+                if (SSUtils.compareDouble(gridVal, NoData)
+                        || SSUtils.compareDouble(gridVal, aoiNoData)) {
+                    column++;
+                    readIter.nextPixel();
+                    continue;
+                }
+
+                Coordinate worldPos = aoiTrans.gridToWorldCoordinate(column, row);
+                GridCoordinates2D gridPos = inputTrans.worldToGrid(worldPos.x, worldPos.y);
+                if (inputTrans.contains(gridPos.x, gridPos.y)) {
+                    outputRaster.setSample(gridPos.x, gridPos.y, 0, gridVal);
+                }
+
+                // TODO
+                updateStatistics(gridVal);
+
+                valid++;
+                column++;
+                readIter.nextPixel();
+            }
+
+            row++;
+            readIter.nextLine();
+        }
+
+        if (valid == 0) {
+            return inputCoverage;
+        }
+
+        outputImage.setData(outputRaster);
+
+        return createGridCoverage(inputCoverage.getName(), outputImage);
+    }
+}

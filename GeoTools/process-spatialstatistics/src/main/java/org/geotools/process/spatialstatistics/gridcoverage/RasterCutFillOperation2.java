aca38571e922e5b3a862871a12a75e98e5eb0a87
mapplus <mapplus@gmail.com>
2020-12-21 20:43:08 +0900

    Update raster cut fill 2

--- GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/gridcoverage/RasterCutFillOperation2.java
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/gridcoverage/RasterCutFillOperation2.java
@@ -23,21 +23,18 @@ import java.util.logging.Logger;
 import javax.media.jai.PlanarImage;
 import javax.media.jai.iterator.RectIter;
 import javax.media.jai.iterator.RectIterFactory;
+import javax.media.jai.iterator.WritableRectIter;
 
 import org.geotools.coverage.grid.GridCoverage2D;
 import org.geotools.coverage.grid.GridGeometry2D;
-import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.feature.simple.SimpleFeatureBuilder;
 import org.geotools.process.ProcessException;
-import org.geotools.process.spatialstatistics.core.FeatureTypes;
 import org.geotools.process.spatialstatistics.core.SSUtils;
+import org.geotools.process.spatialstatistics.enumeration.RasterPixelType;
 import org.geotools.referencing.CRS;
 import org.geotools.util.logging.Logging;
+import org.jaitools.tiledimage.DiskMemImage;
 import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.Polygon;
-import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
@@ -47,7 +44,7 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
  * 
  * @source $URL$
  */
-public class RasterCutFillOperation2 extends RasterProcessingOperation {
+public class RasterCutFillOperation2 extends AbstractRasterCutFillOperation {
     protected static final Logger LOGGER = Logging.getLogger(RasterCutFillOperation2.class);
 
     public RasterCutFillOperation2() {
@@ -79,92 +76,74 @@ public class RasterCutFillOperation2 extends RasterProcessingOperation {
         double beforeX = Math.abs(beforeTrans.getScaleX());
         double beforeY = Math.abs(beforeTrans.getScaleY());
 
+        final int outputNoData = -9999;
         final double cellArea = beforeX * beforeY;
         CutFillResult result = new CutFillResult(0d);
 
+        DiskMemImage outputImage = createDiskMemImage(beforeGc, RasterPixelType.SHORT);
         PlanarImage inputImage = (PlanarImage) beforeGc.getRenderedImage();
         PlanarImage afterImage = (PlanarImage) afterGc.getRenderedImage();
 
         RectIter beforeIter = RectIterFactory.create(inputImage, inputImage.getBounds());
         RectIter afterIter = RectIterFactory.create(afterImage, afterImage.getBounds());
+        WritableRectIter writerIter = RectIterFactory.createWritable(outputImage,
+                outputImage.getBounds());
 
         beforeIter.startLines();
         afterIter.startLines();
-        while (!beforeIter.finishedLines() && !afterIter.finishedLines()) {
+        writerIter.startLines();
+        while (!beforeIter.finishedLines() && !afterIter.finishedLines()
+                && !writerIter.finishedLines()) {
             beforeIter.startPixels();
             afterIter.startPixels();
+            writerIter.startPixels();
 
-            while (!beforeIter.finishedPixels() && !afterIter.finishedPixels()) {
+            while (!beforeIter.finishedPixels() && !afterIter.finishedPixels()
+                    && !writerIter.finishedPixels()) {
                 final double beforeVal = beforeIter.getSampleDouble(0);
                 final double afterVal = afterIter.getSampleDouble(0);
 
-                if (SSUtils.compareDouble(beforeNoData, beforeVal)
-                        || SSUtils.compareDouble(afaterNoData, afterVal)) {
-                    beforeIter.nextPixel();
-                    afterIter.nextPixel();
-                    continue;
-                }
-
-                double diffVal = beforeVal - afterVal;
-                double volume = Math.abs(cellArea * diffVal);
-
                 // Cut = 1, Fill = -1, Unchanged = 0
-                if (diffVal > 0) {
-                    result.cutArea += cellArea;
-                    result.cutVolume += volume;
-                    result.cutCount += 1;
-                } else if (diffVal < 0) {
-                    result.fillArea += cellArea;
-                    result.fillVolume += volume;
-                    result.fillCount += 1;
-                } else {
-                    result.unChangedArea += cellArea;
-                    result.unChangedCount += 1;
+                int flag = outputNoData;
+
+                if (!SSUtils.compareDouble(beforeNoData, beforeVal)
+                        && !SSUtils.compareDouble(afaterNoData, afterVal)) {
+                    double diffVal = beforeVal - afterVal;
+                    double volume = Math.abs(cellArea * diffVal);
+
+                    if (diffVal > 0) {
+                        result.cutArea += cellArea;
+                        result.cutVolume += volume;
+                        result.cutCount += 1;
+                        flag = 1;
+                    } else if (diffVal < 0) {
+                        result.fillArea += cellArea;
+                        result.fillVolume += volume;
+                        result.fillCount += 1;
+                        flag = -1;
+                    } else {
+                        result.unChangedArea += cellArea;
+                        result.unChangedCount += 1;
+                        flag = 0;
+                    }
                 }
+
+                writerIter.setSample(0, flag);
 
                 beforeIter.nextPixel();
                 afterIter.nextPixel();
+                writerIter.nextPixel();
             }
 
             beforeIter.nextLine();
             afterIter.nextLine();
+            writerIter.nextLine();
         }
+
+        cutFillRaster = createGridCoverage("CutFill", outputImage, 0, outputNoData, -1, 1, Extent);
 
         // finally build features
-        return buildFeatures(result, beforeDEM.getCoordinateReferenceSystem());
-    }
-
-    private SimpleFeatureCollection buildFeatures(CutFillResult result,
-            CoordinateReferenceSystem crs) throws ProcessException, IOException {
-        // create schema
-        SimpleFeatureType schema = FeatureTypes.getDefaultType("CutFill", Polygon.class, crs);
-        schema = FeatureTypes.add(schema, "category", Integer.class);
-        schema = FeatureTypes.add(schema, "count", Integer.class);
-        schema = FeatureTypes.add(schema, "area", Double.class);
-        schema = FeatureTypes.add(schema, "volume", Double.class);
-
-        // create features
-        ListFeatureCollection outputFc = new ListFeatureCollection(schema);
-        SimpleFeatureBuilder builder = new SimpleFeatureBuilder(schema);
-
-        int serialID = 0;
-        for (int category = -1; category <= 1; category++) {
-            double area = result.getArea(category);
-            double volume = result.getVolume(category);
-            int count = result.getCount(category);
-
-            String fid = schema.getTypeName() + "." + (++serialID);
-            SimpleFeature newFeature = builder.buildFeature(fid);
-
-            newFeature.setAttribute("category", category);
-            newFeature.setAttribute("count", count);
-            newFeature.setAttribute("area", area);
-            newFeature.setAttribute("volume", volume);
-
-            outputFc.add(newFeature);
-        }
-
-        return outputFc;
+        return buildFeatures(cutFillRaster, result);
     }
 
     private boolean validateProperties(GridCoverage2D beforeDEM, GridCoverage2D afterDEM) {
@@ -191,59 +170,5 @@ public class RasterCutFillOperation2 extends RasterProcessingOperation {
         }
 
         return true;
-    }
-
-    static final class CutFillResult {
-        public Double baseHeight = Double.NaN;
-
-        public Double cutArea = Double.valueOf(0);
-
-        public Double fillArea = Double.valueOf(0);
-
-        public Double unChangedArea = Double.valueOf(0);
-
-        public Double cutVolume = Double.valueOf(0);
-
-        public Double fillVolume = Double.valueOf(0);
-
-        public Integer cutCount = Integer.valueOf(0);
-
-        public Integer fillCount = Integer.valueOf(0);
-
-        public Integer unChangedCount = Integer.valueOf(0);
-
-        public CutFillResult(Double baseHeight) {
-            this.baseHeight = baseHeight;
-        }
-
-        public double getArea(int category) {
-            if (category == -1) { // fill
-                return fillArea;
-            } else if (category == 1) { // cut
-                return cutArea;
-            } else { // unchanged
-                return unChangedArea;
-            }
-        }
-
-        public double getVolume(int category) {
-            if (category == -1) { // fill
-                return fillVolume;
-            } else if (category == 1) { // cut
-                return cutVolume;
-            } else { // unchanged
-                return 0d;
-            }
-        }
-
-        public int getCount(int category) {
-            if (category == -1) { // fill
-                return fillCount;
-            } else if (category == 1) { // cut
-                return cutCount;
-            } else { // unchanged
-                return unChangedCount;
-            }
-        }
     }
 }

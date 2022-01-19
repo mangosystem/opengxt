0a910bdce4bf99397acd63d8a194f7ebaf2e5b35
mapplus <mapplus@gmail.com>
2022-01-19 17:39:32 +0900

    Surface Area

---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterSurfaceAreaProcess.java
@@ -0,0 +1,99 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2022, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.process.Process;
+import org.geotools.process.ProcessException;
+import org.geotools.process.ProcessFactory;
+import org.geotools.process.spatialstatistics.core.Params;
+import org.geotools.process.spatialstatistics.gridcoverage.RasterSurfaceAreaOperation;
+import org.geotools.util.logging.Logging;
+import org.locationtech.jts.geom.Geometry;
+import org.opengis.util.ProgressListener;
+
+/**
+ * Calculates the surface area in polygons using DEM.
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterSurfaceAreaProcess extends AbstractStatisticsProcess {
+    protected static final Logger LOGGER = Logging.getLogger(RasterSurfaceAreaProcess.class);
+
+    public RasterSurfaceAreaProcess(ProcessFactory factory) {
+        super(factory);
+    }
+
+    public ProcessFactory getFactory() {
+        return factory;
+    }
+
+    public static SimpleFeatureCollection process(GridCoverage2D inputCoverage, Geometry cropShape,
+            ProgressListener monitor) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(RasterSurfaceAreaProcessFactory.inputCoverage.key, inputCoverage);
+        map.put(RasterSurfaceAreaProcessFactory.cropShape.key, cropShape);
+
+        Process process = new RasterSurfaceAreaProcess(null);
+        Map<String, Object> resultMap;
+        try {
+            resultMap = process.execute(map, monitor);
+
+            return (SimpleFeatureCollection) resultMap
+                    .get(RasterSurfaceAreaProcessFactory.RESULT.key);
+        } catch (ProcessException e) {
+            LOGGER.log(Level.FINER, e.getMessage(), e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input, ProgressListener monitor)
+            throws ProcessException {
+        GridCoverage2D inputCoverage = (GridCoverage2D) Params.getValue(input,
+                RasterSurfaceAreaProcessFactory.inputCoverage, null);
+        Geometry cropShape = (Geometry) Params.getValue(input,
+                RasterSurfaceAreaProcessFactory.cropShape, null);
+        if (inputCoverage == null || cropShape == null) {
+            throw new NullPointerException("inputCoverage, cropShape parameters required");
+        }
+
+        // start process
+        SimpleFeatureCollection resultFc = null;
+        try {
+            RasterSurfaceAreaOperation cutfillOperation = new RasterSurfaceAreaOperation();
+            resultFc = cutfillOperation.execute(inputCoverage, cropShape);
+        } catch (IOException e) {
+            throw new ProcessException(e);
+        }
+        // end process
+
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(RasterSurfaceAreaProcessFactory.RESULT.key, resultFc);
+        return resultMap;
+    }
+}
---
+++ GeoTools/process-spatialstatistics/src/main/java/org/geotools/process/spatialstatistics/RasterSurfaceAreaProcessFactory.java
@@ -0,0 +1,105 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2022, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.process.spatialstatistics;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.data.Parameter;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.NameImpl;
+import org.geotools.process.Process;
+import org.geotools.util.logging.Logging;
+import org.locationtech.jts.geom.Geometry;
+import org.opengis.util.InternationalString;
+
+/**
+ * RasterSurfaceAreaProcessFactory
+ * 
+ * @author Minpa Lee, MangoSystem
+ * 
+ * @source $URL$
+ */
+public class RasterSurfaceAreaProcessFactory extends SpatialStatisticsProcessFactory {
+    protected static final Logger LOGGER = Logging.getLogger(RasterSurfaceAreaProcessFactory.class);
+
+    private static final String PROCESS_NAME = "RasterSurfaceArea";
+
+    /*
+     * RasterSurfaceArea(GridCoverage2D inputDEM, Geometry cropShape): SimpleFeatureCollection
+     */
+
+    public RasterSurfaceAreaProcessFactory() {
+        super(new NameImpl(NAMESPACE, PROCESS_NAME));
+    }
+
+    @Override
+    public Process create() {
+        return new RasterCutFillProcess(this);
+    }
+
+    @Override
+    public InternationalString getTitle() {
+        return getResource("RasterSurfaceArea.title");
+    }
+
+    @Override
+    public InternationalString getDescription() {
+        return getResource("RasterSurfaceArea.description");
+    }
+
+    /** inputCoverage */
+    public static final Parameter<GridCoverage2D> inputCoverage = new Parameter<GridCoverage2D>(
+            "inputCoverage", GridCoverage2D.class,
+            getResource("RasterSurfaceArea.inputCoverage.title"),
+            getResource("RasterSurfaceArea.inputCoverage.description"), true, 1, 1, null, null);
+
+    /** cropShape */
+    public static final Parameter<Geometry> cropShape = new Parameter<Geometry>("cropShape",
+            Geometry.class, getResource("RasterSurfaceArea.cropShape.title"),
+            getResource("RasterSurfaceArea.cropShape.description"), true, 1, 1, null, null);
+
+    @Override
+    protected Map<String, Parameter<?>> getParameterInfo() {
+        HashMap<String, Parameter<?>> parameterInfo = new LinkedHashMap<String, Parameter<?>>();
+        parameterInfo.put(inputCoverage.key, inputCoverage);
+        parameterInfo.put(cropShape.key, cropShape);
+        return parameterInfo;
+    }
+
+    /** result */
+    public static final Parameter<SimpleFeatureCollection> RESULT = new Parameter<SimpleFeatureCollection>(
+            "result", SimpleFeatureCollection.class, getResource("RasterSurfaceArea.result.title"),
+            getResource("RasterSurfaceArea.result.description"));
+
+    static final Map<String, Parameter<?>> resultInfo = new TreeMap<String, Parameter<?>>();
+    static {
+        resultInfo.put(RESULT.key, RESULT);
+    }
+
+    @Override
+    protected Map<String, Parameter<?>> getResultInfo(Map<String, Object> parameters)
+            throws IllegalArgumentException {
+        return Collections.unmodifiableMap(resultInfo);
+    }
+
+}

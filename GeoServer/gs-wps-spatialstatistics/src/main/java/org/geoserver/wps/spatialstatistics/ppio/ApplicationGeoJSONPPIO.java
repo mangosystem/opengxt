5d809b3ae68afde1686b0957beb1c4a1b8748c3f
mapplus <mapplus@gmail.com>
2023-02-16 07:29:09 +0900

    FeatureJSON to GeoJSONWriter

--- GeoServer/gs-wps-spatialstatistics/src/main/java/org/geoserver/wps/spatialstatistics/ppio/ApplicationGeoJSONPPIO.java
+++ GeoServer/gs-wps-spatialstatistics/src/main/java/org/geoserver/wps/spatialstatistics/ppio/ApplicationGeoJSONPPIO.java
@@ -8,12 +8,20 @@ package org.geoserver.wps.spatialstatistics.ppio;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
 import org.geoserver.wps.ppio.CDataPPIO;
+import org.geotools.data.geojson.GeoJSONReader;
+import org.geotools.data.geojson.GeoJSONWriter;
+import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.feature.FeatureCollection;
-import org.geotools.geojson.feature.FeatureJSON;
-import org.geotools.geojson.geom.GeometryJSON;
+import org.geotools.util.logging.Logging;
 import org.locationtech.jts.geom.Geometry;
+
+import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Inputs and outputs feature collections in GeoJSON format using gt-geojson
@@ -24,8 +32,19 @@ import org.locationtech.jts.geom.Geometry;
  * @source $URL$
  */
 public abstract class ApplicationGeoJSONPPIO extends CDataPPIO {
+    protected static final Logger LOGGER = Logging.getLogger(ApplicationGeoJSONPPIO.class);
 
     static final int DECIMALS = 10; // for EPSG:4326
+
+    private static JsonFactory JSON_FACTORY = new JsonFactory();;
+
+    static final ObjectMapper MAPPER;
+
+    static {
+        MAPPER = new ObjectMapper();
+        JtsModule module = new JtsModule(DECIMALS);
+        MAPPER.registerModule(module);
+    }
 
     @SuppressWarnings("rawtypes")
     public ApplicationGeoJSONPPIO(Class clazz) {
@@ -52,24 +71,26 @@ public abstract class ApplicationGeoJSONPPIO extends CDataPPIO {
             super(FeatureCollection.class);
         }
 
-        @SuppressWarnings("rawtypes")
         @Override
         public void encode(Object value, OutputStream os) throws IOException {
-            FeatureJSON json = new FeatureJSON(new GeometryJSON(DECIMALS));
-            // commented out due to GEOT-3209
-            // json.setEncodeFeatureCRS(true);
-            // json.setEncodeFeatureCollectionCRS(true);
-            json.writeFeatureCollection((FeatureCollection) value, os);
+            try (GeoJSONWriter writer = new GeoJSONWriter(os)) {
+                writer.setMaxDecimals(DECIMALS);
+                writer.writeFeatureCollection((SimpleFeatureCollection) value);
+            }
         }
 
         @Override
         public Object decode(InputStream input) throws Exception {
-            return new FeatureJSON(new GeometryJSON(DECIMALS)).readFeatureCollection(input);
+            try (GeoJSONReader reader = new GeoJSONReader(input)) {
+                return reader.getFeatures();
+            }
         }
 
         @Override
         public Object decode(String input) throws Exception {
-            return new FeatureJSON(new GeometryJSON(DECIMALS)).readFeatureCollection(input);
+            try (GeoJSONReader reader = new GeoJSONReader(input)) {
+                return reader.getFeatures();
+            }
         }
     }
 
@@ -81,18 +102,32 @@ public abstract class ApplicationGeoJSONPPIO extends CDataPPIO {
 
         @Override
         public void encode(Object value, OutputStream os) throws IOException {
-            GeometryJSON json = new GeometryJSON(DECIMALS);
-            json.write((Geometry) value, os);
+            ObjectMapper mapper = getMapper(DECIMALS);
+            mapper.writeValue(os, value);
+        }
+
+        private ObjectMapper getMapper(int decimals) {
+            if (decimals == DECIMALS) {
+                return MAPPER;
+            }
+            ObjectMapper mapper = new ObjectMapper();
+            JtsModule module = new JtsModule(decimals);
+            mapper.registerModule(module);
+            return mapper;
         }
 
         @Override
         public Object decode(InputStream input) throws Exception {
-            return new GeometryJSON(DECIMALS).read(input);
+            try (JsonParser parser = JSON_FACTORY.createParser(input)) {
+                return MAPPER.readValue(parser, Geometry.class);
+            }
         }
 
         @Override
         public Object decode(String input) throws Exception {
-            return new GeometryJSON(DECIMALS).read(input);
+            try (JsonParser parser = JSON_FACTORY.createParser(input)) {
+                return MAPPER.readValue(parser, Geometry.class);
+            }
         }
     }
 }
--- GeoServer/gs-wps-spatialstatistics/src/main/java/org/geoserver/wps/spatialstatistics/ppio/GeometryGeoJSONPPIO.java
+++ GeoServer/gs-wps-spatialstatistics/src/main/java/org/geoserver/wps/spatialstatistics/ppio/GeometryGeoJSONPPIO.java
@@ -7,47 +7,35 @@ package org.geoserver.wps.spatialstatistics.ppio;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.StringReader;
-import java.io.Writer;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.geoserver.wps.ppio.CDataPPIO;
-import org.geotools.geojson.geom.GeometryJSON;
 import org.geotools.util.logging.Logging;
 import org.locationtech.jts.geom.Geometry;
+
+import com.bedatadriven.jackson.datatype.jts.JtsModule;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GeometryGeoJSONPPIO extends CDataPPIO {
     protected static final Logger LOGGER = Logging.getLogger(GeometryGeoJSONPPIO.class);
 
+    static final int DECIMALS = 10; // for EPSG:4326
+
+    private static JsonFactory JSON_FACTORY = new JsonFactory();;
+
+    static final ObjectMapper MAPPER;
+
+    static {
+        MAPPER = new ObjectMapper();
+        JtsModule module = new JtsModule(DECIMALS);
+        MAPPER.registerModule(module);
+    }
+
     protected GeometryGeoJSONPPIO() {
-        super(Geometry.class, Geometry.class, "application/json");
-    }
-
-    @Override
-    public Object decode(InputStream input) throws Exception {
-        return new GeometryJSON().read(new InputStreamReader(input));
-    }
-
-    @Override
-    public Object decode(String input) throws Exception {
-        return new GeometryJSON().read(new StringReader(input));
-    }
-
-    @Override
-    public void encode(Object value, OutputStream os) throws IOException {
-        Writer writer = new OutputStreamWriter(os);
-        try {
-            GeometryJSON geometryJson = new GeometryJSON();
-            geometryJson.write((Geometry) value, writer);
-        } catch (IOException e) {
-            LOGGER.log(Level.FINER, e.getMessage(), e);
-        } finally {
-            writer.flush();
-        }
+        super(Geometry.class, Geometry.class, "application/vnd.geo+json");
     }
 
     @Override
@@ -55,4 +43,33 @@ public class GeometryGeoJSONPPIO extends CDataPPIO {
         return "json";
     }
 
+    @Override
+    public void encode(Object value, OutputStream os) throws IOException {
+        ObjectMapper mapper = getMapper(DECIMALS);
+        mapper.writeValue(os, value);
+    }
+
+    private ObjectMapper getMapper(int decimals) {
+        if (decimals == DECIMALS) {
+            return MAPPER;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        JtsModule module = new JtsModule(decimals);
+        mapper.registerModule(module);
+        return mapper;
+    }
+
+    @Override
+    public Object decode(InputStream input) throws Exception {
+        try (JsonParser parser = JSON_FACTORY.createParser(input)) {
+            return MAPPER.readValue(parser, Geometry.class);
+        }
+    }
+
+    @Override
+    public Object decode(String input) throws Exception {
+        try (JsonParser parser = JSON_FACTORY.createParser(input)) {
+            return MAPPER.readValue(parser, Geometry.class);
+        }
+    }
 }
